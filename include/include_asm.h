7bbbdafd35e2e51f53b5736ea821e5b52d1dac20
Vatuu <21113232+Vatuu@users.noreply.github.com>
2024-05-09 16:19:06 +0200

    Finally ecided on a decent structure

--- include/include_asm.h
+++ include/include_asm.h
@@ -1,28 +1,44 @@
-#ifndef __INCLUDE_ASM_H__
-#define __INCLUDE_ASM_H__
+#ifndef INCLUDE_ASM_H
+#define INCLUDE_ASM_H
 
-#if !defined(SPLAT) && !defined(__CTX__) && !defined(PERMUTER)
-
+#if !defined(SPLAT) && !defined(M2CTX) && !defined(PERMUTER)
 #ifndef INCLUDE_ASM
-#define INCLUDE_ASM(FOLDER, NAME, ...) \
+#define INCLUDE_ASM(FOLDER, NAME) \
     __asm__( \
         ".section .text\n" \
+        "\t.set noat\n" \
+        "\t.set noreorder\n" \
         "\t.align\t2\n" \
         "\t.globl\t"#NAME"\n" \
+        "\t.type "#NAME", @function\n" \
         "\t.ent\t"#NAME"\n" \
         #NAME ":\n" \
-        "\t.include \"" FOLDER "/" #NAME ".s\"\n" \
+        "\t.include \""FOLDER"/"#NAME".s\"\n" \
         "\t.set reorder\n" \
         "\t.set at\n" \
-        "\t.end\t"#NAME);
+        "\t.end\t"#NAME"\n" \
+        ".end"#NAME":\n" \
+        "\t.size\t"#NAME",.end"#NAME"-"#NAME "\n" \
+        "\t.globl\t" #NAME ".NON_MATCHING\n" \
+        "\t" #NAME ".NON_MATCHING" " = " #NAME "\n" \
+    )
 #endif
-
-__asm__(".include \"include/macro.inc\"\n");
-
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA(FOLDER, NAME) \
+    __asm__( \
+        ".section .rodata\n" \
+        "\t.include \""FOLDER"/"#NAME".s\"\n" \
+        ".section .text" \
+    )
+#endif
+__asm__(".include \"include/labels.inc\"\n");
 #else
-
-#define INCLUDE_ASM(FOLDER, NAME, ...)
-
+#ifndef INCLUDE_ASM
+#define INCLUDE_ASM(FOLDER, NAME)
+#endif
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA(FOLDER, NAME)
+#endif
 #endif
 
 #endif
