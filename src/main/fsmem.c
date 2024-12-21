2fac4e1bdff27852d5764a63d99b57cfe0e7757f
fgsfds <derp.primus@gmail.com>
2024-12-21 14:53:13 +0100

    fsmem: decompile fsMemInit

--- src/main/fsmem.c
+++ src/main/fsmem.c
@@ -1,6 +1,36 @@
 #include "main/fsmem.h"
 
-INCLUDE_ASM("asm/main/nonmatchings/fsmem", fsMemInit);
+void fsMemInit(u8 *start, u32 size) {
+  s32 i;
+
+  // this really wants the start and size to be aligned to 8,
+  // but since they're already hardcoded elsewhere, it doesn't really matter
+  if ((u32)start & 3) {
+    size = 0;
+  }
+  if (size & 3) {
+    size = 0;
+  }
+
+  g_FsMem.start = start;
+  g_FsMem.size = size;
+  g_FsMem.alloc_list.next = NULL;
+  g_FsMem.alloc_list.start = start;
+  g_FsMem.alloc_list.size = 0;
+  g_FsMem.free_list.next = g_FsMem.blocks;
+  g_FsMem.free_list.start = NULL;
+  g_FsMem.free_list.size = 0;
+
+  for (i = 0; i < FS_MEM_NUM_BLOCKS; i++) {
+    g_FsMem.blocks[i].start = NULL;
+    g_FsMem.blocks[i].size = 0;
+    if (i == FS_MEM_NUM_BLOCKS - 1) {
+      g_FsMem.blocks[i].next = NULL;
+    } else {
+      g_FsMem.blocks[i].next = &g_FsMem.blocks[i + 1];
+    }
+  }
+}
 
 void nullsub_80011cfc(void) {
 
