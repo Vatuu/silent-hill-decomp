95e2b405328d16c05d16004ea83b5e4ac25490d4
fgsfds <derp.primus@gmail.com>
2024-12-24 02:03:25 +0100

    fsqueue_2: better match for fsQueueUpdateRead

--- src/main/fsqueue_2.c
+++ src/main/fsqueue_2.c
@@ -9,91 +9,74 @@ s32 fsQueueUpdateRead(FsQueueEntry* entry) {
 
   switch (g_FsQueue.state) {
     case FSQS_READ_ALLOCATE:
-      status = fsQueueAllocEntryData(entry);
-      // these double checks seem to be required for a match
-      if (status == 0) {
-        // I guess this will just retry until memory is allocated?
-        break;
-      }
-      if (status == 1) {
-        g_FsQueue.state = FSQS_READ_CHECK;
+      switch (fsQueueAllocEntryData(entry)) {
+        case 0:
+          // I guess this will just retry until memory is allocated?
+          break;
+        case 1:
+          g_FsQueue.state = FSQS_READ_CHECK;
+          break;
       }
       break;
 
     case FSQS_READ_CHECK:
-      status = fsQueueCanRead(entry);
-      if (status == 0) {
-        // can't read yet, memory is in use by another operation, wait until next tick
-        break;
-      }
-      if (status == 1) {
-        g_FsQueue.state = FSQS_READ_SETLOC;
+      switch (fsQueueCanRead(entry)) {
+        case 0:
+          // can't read yet, memory is in use by another operation, wait until next tick
+          break;
+        case 1:
+          g_FsQueue.state = FSQS_READ_SETLOC;
+          break;
       }
       break;
 
     case FSQS_READ_SETLOC:
-      status = fsQueueTickSetLoc(entry);
-      // @FIXME: can't figure out how to get this and other cases to match otherwise,
-      // no if/else/break/whatever combination seems to work, but this does
-      if (status == 0) {
-        // CdlSetloc failed, reset CD
-        goto _setloc_fail;
+      switch (fsQueueTickSetLoc(entry)) {
+        case 0:
+          // CdlSetloc failed, reset CD
+          g_FsQueue.state = FSQS_READ_RESET;
+          break;
+        case 1:
+          g_FsQueue.state = FSQS_READ_READ;
+          break;
       }
-      if (status == 1) {
-        goto _setloc_ok;
-      }
-      break;
-_setloc_fail:
-      g_FsQueue.state = FSQS_READ_RESET;
-      break;
-_setloc_ok:
-      g_FsQueue.state = FSQS_READ_READ;
       break;
 
     case FSQS_READ_READ:
-      status = fsQueueTickRead(entry);
-      if (status == 0) {
-        // CdRead failed, reset CD
-        goto _read_fail;
+      switch (fsQueueTickRead(entry)) {
+        case 0:
+          // CdRead failed, reset CD and retry
+          g_FsQueue.state = FSQS_READ_RESET;
+          break;
+        case 1:
+          g_FsQueue.state = FSQS_READ_SYNC;
+          break;
       }
-      if (status == 1) {
-        goto _read_ok;
-      }
-      break;
-_read_fail:
-      g_FsQueue.state = FSQS_READ_RESET;
-      break;
-_read_ok:
-      g_FsQueue.state = FSQS_READ_SYNC;
       break;
 
     case FSQS_READ_SYNC:
       // check how the read is going
-      status = CdReadSync(1, NULL);
-      if (status == -1) {
-        // CdReadSync failed, reset CD
-        goto _sync_fail;
+      switch (CdReadSync(1, NULL)) {
+        case -1:
+          // CdReadSync failed, reset CD
+          g_FsQueue.state = FSQS_READ_RESET;
+          break;
+        case 0:
+          // done reading, no state transition, let caller know that we're done
+          result = 1;
+          break;
       }
-      if (status == 0) {
-        // finished reading, no state transition
-        goto _sync_ok;
-      }
-      break;
-_sync_fail:
-      g_FsQueue.state = FSQS_READ_RESET;
-      break;
-_sync_ok:
-      // finished reading, no state transition, let caller know that we're done
-      result = 1;
       break;
 
     case FSQS_READ_RESET:
-      status = fsQueueTickReset(entry);
-      if (status == 0) {
-        break;
-      }
-      if (status == 1) {
-        g_FsQueue.state = FSQS_READ_SETLOC;
+      switch (fsQueueTickReset(entry)) {
+        case 0:
+          // still resetting
+          break;
+        case 1:
+          // reset done, try again from the setloc
+          g_FsQueue.state = FSQS_READ_SETLOC;
+          break;
       }
       break;
   }
