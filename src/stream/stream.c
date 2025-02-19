5285c436e4723a90fe28f8f20ce1599144553df5
Sezz <sezzary@outlook.com>
2025-02-19 23:21:16 +1100

    Generate non-matching calls

---
+++ src/stream/stream.c
@@ -0,0 +1,77 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2654);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E279C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2838);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E28B0);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2908);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2A24);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2AA4);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2B9C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2F8C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E300C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E307C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E31CC);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3298);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E331C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3438);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E34CC);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E34E0);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E34F4);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3508);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E352C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3560);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E35EC);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3684);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3690);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E370C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E372C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3768);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E37A4);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E37C8);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E37EC);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E38DC);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E396C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E39F8);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3A8C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3B20);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3B38);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3BBC);
+
+INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3BEC);
