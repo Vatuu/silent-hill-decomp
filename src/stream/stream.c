9b443ae0737f42a48f4f148d2cb61d020d676b60
Sezz <sezzary@outlook.com>
2025-02-20 02:12:18 +1100

    stream.yaml form emoose's fork

--- src/stream/stream.c
+++ src/stream/stream.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+#include <LIBDS.H>
+#include <LIBPRESS.H>
+
 INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E2654);
 
 INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E279C);
@@ -29,49 +32,3 @@ INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3298);
 INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E331C);
 
 INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3438);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E34CC);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E34E0);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E34F4);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3508);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E352C);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3560);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E35EC);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3684);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3690);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E370C);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E372C);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3768);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E37A4);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E37C8);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E37EC);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E38DC);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E396C);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E39F8);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3A8C);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3B20);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3B38);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3BBC);
-
-INCLUDE_ASM("asm/stream/nonmatchings/stream", func_801E3BEC);
