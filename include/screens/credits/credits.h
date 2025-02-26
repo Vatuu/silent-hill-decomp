b14e370aef7cd0220281479bbd801bfc3be753c0
Sezz <sezzary@outlook.com>
2025-02-26 15:25:22 +1100

    Fix embarrassing doc type

--- include/screens/credits/credits.h
+++ include/screens/credits/credits.h
@@ -11,7 +11,7 @@ s32 func_801E3DF8(s32 arg0);
  * @brief Sets the current RGB+command color for D_800AFE10.
  *
  * This function sets the packed RGB+command color for D_800AFE10 with
- * the forth component hard set to 0x64, possibly a graphics command.
+ * the fourth component hard set to 0x64, possibly a graphics command.
  * RGB order only assumed.
  *
  * @param r Red component.
@@ -26,7 +26,7 @@ void func_801E4340(s8 arg0);
  * @brief Sets the current RGB+command color for D_800AFE2C.
  *
  * This function sets the packed RGB+command color for D_800AFE10 with
- * the forth component hard set to 0x2C, possibly a graphics command.
+ * the fourth component hard set to 0x2C, possibly a graphics command.
  * RGB order only assumed.
  *
  * @param r Red component.
