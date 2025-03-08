c4a19477e9afc57e20f1bb1bb146bb1b3cfd97e7
Vatuu <21113232+Vatuu@users.noreply.github.com>
2025-03-08 12:15:23 +0100

    Revert "objdiff Reporting"
    This reverts commit 2f415cc578a1be9b588b857fde830bd73fc96e2d.

--- include/include_asm.h
+++ include/include_asm.h
@@ -1,43 +1,44 @@
 #ifndef INCLUDE_ASM_H
 #define INCLUDE_ASM_H
 
-#if !defined(SPLAT) && !defined(M2CTX) && !defined(PERMUTER) && !defined(SKIP_ASM)
+#if !defined(SPLAT) && !defined(M2CTX) && !defined(PERMUTER)
+#ifndef INCLUDE_ASM
+#define INCLUDE_ASM(FOLDER, NAME)                                              \
+  __asm__(".section .text\n"                                                   \
+          "\t.set noat\n"                                                      \
+          "\t.set noreorder\n"                                                 \
+          "\t.align\t2\n"                                                      \
+          "\t.globl\t" #NAME "\n"                                              \
+          "\t.type " #NAME ", @function\n"                                     \
+          "\t.ent\t" #NAME "\n" #NAME ":\n"                                    \
+          "\t.include \"" FOLDER "/" #NAME ".s\"\n"                            \
+          "\t.set reorder\n"                                                   \
+          "\t.set at\n"                                                        \
+          "\t.end\t" #NAME "\n"                                                \
+          ".end" #NAME ":\n"                                                   \
+          "\t.size\t" #NAME ",.end" #NAME "-" #NAME "\n"                       \
+          "\t.globl\t" #NAME ".NON_MATCHING\n"                                 \
+          "\t" #NAME ".NON_MATCHING"                                           \
+          " = " #NAME "\n")
+#endif
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA(FOLDER, NAME)                                           \
+  __asm__(".section .rodata\n"                                                 \
+          "\t.include \"" FOLDER "/" #NAME ".s\"\n"                            \
+          ".section .text")
+#endif
 
-        #ifndef INCLUDE_ASM
-                #define INCLUDE_ASM(FOLDER, NAME)                                                    \
-                        __asm__(".section .text\n"                                                   \
-                                "\t.set noat\n"                                                      \
-                                "\t.set noreorder\n"                                                 \
-                                "\t.align\t2\n"                                                      \
-                                "\t.globl\t" #NAME "\n"                                              \
-                                "\t.type " #NAME ", @function\n"                                     \
-                                "\t.ent\t" #NAME "\n" #NAME ":\n"                                    \
-                                "\t.include \"" FOLDER "/" #NAME ".s\"\n"                            \
-                                "\t.set reorder\n"                                                   \
-                                "\t.set at\n"                                                        \
-                                "\t.end\t" #NAME "\n"                                                \
-                                ".end" #NAME ":\n"                                                   \
-                                "\t.size\t" #NAME ",.end" #NAME "-" #NAME "\n")
-        #endif
-
-        #ifndef INCLUDE_RODATA
-                #define INCLUDE_RODATA(FOLDER, NAME)                                                 \
-                        __asm__(".section .rodata\n"                                                 \
-                                "\t.include \"" FOLDER "/" #NAME ".s\"\n"                            \
-                                ".section .text")
-        #endif
-
-        __asm__(".include \"macro.inc\"\n"
-                "\t.set\treorder\n"
-                "\t.set\tat\n");
+__asm__(".include \"macro.inc\"\n"
+        "\t.set\treorder\n"
+        "\t.set\tat\n");
 
 #else
-        #ifndef INCLUDE_ASM
-                #define INCLUDE_ASM(FOLDER, NAME)
-        #endif
-        #ifndef INCLUDE_RODATA
-                #define INCLUDE_RODATA(FOLDER, NAME)
-        #endif
+#ifndef INCLUDE_ASM
+#define INCLUDE_ASM(FOLDER, NAME)
+#endif
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA(FOLDER, NAME)
+#endif
 #endif
 
 #endif
