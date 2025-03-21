6c2dd5b7060fbeef5ee8692611f048a54849a2cf
Sezz <sezzary@outlook.com>
2025-03-22 03:35:33 +1100

    Update math.h

--- include/bodyprog/math.h
+++ include/bodyprog/math.h
@@ -32,7 +32,7 @@
 
 /** Converts a floating-point alpha in the range [0.0f, 1.0f] to a fixed-point alpha in Q3.12 format. */
 #define FP_ALPHA(alpha) \
-    (s16)((alpha) * (1 << Q12_SHIFT))
+    (s16)((alpha) * (1 << (Q12_SHIFT)))
 
 /** Converts floating-point degrees to fixed-point angles in Q1.15 format (used at Q4.12 resolution). */
 #define FP_ANGLE(deg) \
