204e1c77cac42c4b2677ce2558845fa8df785a3d
Sezz <sezzary@outlook.com>
2025-04-08 23:13:10 +1000

    Simplify flags

--- src/maps/map0_s00/map0_s00.c
+++ src/maps/map0_s00/map0_s00.c
@@ -361,7 +361,7 @@ void func_800D923C(s_SubCharacter* playerChara)
     s32 flags;
     
     flags = playerChara->flags_E0;
-    flags = flags & ~0xF00;
+    flags = flags & ~((1 << 8) | (1 << 9) | (1 << 10) | (1 << 11));
 
     playerChara->model_0.isAnimStateUnchanged_3 = 0;
     playerChara->field_C8 = 0;
@@ -378,7 +378,7 @@ void func_800D923C(s_SubCharacter* playerChara)
     playerChara->properties_E4[PlayerProperty_PositionY] = 0;
     playerChara->properties_E4[PlayerProperty_Unk3] = 0;
     playerChara->properties_E4[PlayerProperty_Unk4] = 0;
-    playerChara->flags_E0 = flags | 0x300;
+    playerChara->flags_E0 = flags | ((1 << 8) | (1 << 9));
     playerChara->model_0.field_2++;
 }
 
