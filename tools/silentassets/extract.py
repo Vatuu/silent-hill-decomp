e20f8407656b6c8ce38ac982aaf81b8ad2d0f9f2
emoose <abc@cock.li>
2025-04-09 02:32:37 +0100

    silentassets: rename Region -> Release

--- tools/silentassets/extract.py
+++ tools/silentassets/extract.py
@@ -16,7 +16,7 @@ class Flags(IntFlag):
     ALT_FILE_STRUCT = 4
 
 @dataclass
-class Region:
+class Release:
     id: str
     name: str
     checksum: int
@@ -27,14 +27,9 @@ class Region:
     flags: Flags
 
 @dataclass
-class EntryType:
-    name: str 
-    extension: str
-
-@dataclass
 class TableEntry:
     path: Path
-    type: EntryType
+    type: str
     size: int
     offset: int
 
@@ -97,24 +92,24 @@ DIRS_OPM16 = [
     "VIN", "XA"
 ]
 
-REGIONS = (
+RELEASES = (
 # Partial demo releases
-    Region("NTSC OPM16 Demo 98-10-19", "SCUS-94278", 0x2534D4BE, 0xAA90, 886, DIRS_OPM16, FILE_TYPES_OPM16, Flags.ALT_FILE_STRUCT),
-    Region("NTSC-J Trial Version 98-11-06", "SLPM-80363", 0xBE8E95CF, 0xB780, 849, DIRS_DEMO, FILE_TYPES_DEMO, Flags.ALT_FILE_STRUCT),
-    Region("PAL Demo 98-12-16", "SLED-01735", 0x9AE067D4, 0xB648, 850, DIRS_DEMO, FILE_TYPES_DEMO, 0),
-    Region("NTSC Demo 99-01-16", "SLUS-90050", 0x55E85F78, 0xB648, 849, DIRS_DEMO, FILE_TYPES_DEMO, 0),
-    Region("PAL Demo 99-06-08", "SLED-02190", 0x08E0B752, 0xC8FC, 1015, DIRS_PAL, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
-    Region("PAL Demo 99-06-16", "SLED-02186", 0x1782AA0A, 0xB8FC, 1015, DIRS_PAL, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
+    Release("NTSC OPM16 Demo 98-10-19", "SCUS-94278", 0x2534D4BE, 0xAA90, 886, DIRS_OPM16, FILE_TYPES_OPM16, Flags.ALT_FILE_STRUCT),
+    Release("NTSC-J Trial Version 98-11-06", "SLPM-80363", 0xBE8E95CF, 0xB780, 849, DIRS_DEMO, FILE_TYPES_DEMO, Flags.ALT_FILE_STRUCT),
+    Release("PAL Demo 98-12-16", "SLED-01735", 0x9AE067D4, 0xB648, 850, DIRS_DEMO, FILE_TYPES_DEMO, 0),
+    Release("NTSC Demo 99-01-16", "SLUS-90050", 0x55E85F78, 0xB648, 849, DIRS_DEMO, FILE_TYPES_DEMO, 0),
+    Release("PAL Demo 99-06-08", "SLED-02190", 0x08E0B752, 0xC8FC, 1015, DIRS_PAL, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
+    Release("PAL Demo 99-06-16", "SLED-02186", 0x1782AA0A, 0xB8FC, 1015, DIRS_PAL, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
 # Prototypes
-    Region("NTSC Preview 98-11-24", "SLUS-45678", 0x8B88326C, 0xB71C, 1926, DIRS_NTSC, FILE_TYPES, Flags.NO_XA_CONTAINER),
-    Region("NTSC Review 99-01-07", "SLUS-00707", 0xCC454534, 0xB7B8, 2076, DIRS_NTSC, FILE_TYPES, Flags.NO_XA_CONTAINER),
-    Region("NTSC Trade Demo 99-01-17", "SLUS-80707", 0xC9FFEF2A, 0xB850, 2040, DIRS_NTSC, FILE_TYPES, 0),
-    Region("NTSC Beta 99-01-22", "SLUS-00707", 0x84AB9750, 0xB850, 2072, DIRS_NTSC, FILE_TYPES, 0),
+    Release("NTSC Preview 98-11-24", "SLUS-45678", 0x8B88326C, 0xB71C, 1926, DIRS_NTSC, FILE_TYPES, Flags.NO_XA_CONTAINER),
+    Release("NTSC Review 99-01-07", "SLUS-00707", 0xCC454534, 0xB7B8, 2076, DIRS_NTSC, FILE_TYPES, Flags.NO_XA_CONTAINER),
+    Release("NTSC Trade Demo 99-01-17", "SLUS-80707", 0xC9FFEF2A, 0xB850, 2040, DIRS_NTSC, FILE_TYPES, 0),
+    Release("NTSC Beta 99-01-22", "SLUS-00707", 0x84AB9750, 0xB850, 2072, DIRS_NTSC, FILE_TYPES, 0),
 # Final releases
-    Region("NTSC-J Rev 0 99-01-26", "SLPM-86192", 0x1532C55C, 0xB91C, 2074, DIRS_NTSC, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
-    Region("NTSC 1.1 99-02-10", "SLUS-00707", 0xCF9CD8E5, 0xB91C, 2074, DIRS_NTSC, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
-    Region("NTSC-J Rev 1/Rev 2 99-06-02", "SLPM-86192", 0xEB733CAA, 0xB91C, 2074, DIRS_NTSC, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
-    Region("PAL 99-06-07", "SLES-01514", 0x337E4A60, 0xB8FC, 2310, DIRS_PAL, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
+    Release("NTSC-J Rev 0 99-01-26", "SLPM-86192", 0x1532C55C, 0xB91C, 2074, DIRS_NTSC, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
+    Release("NTSC 1.1 99-02-10", "SLUS-00707", 0xCF9CD8E5, 0xB91C, 2074, DIRS_NTSC, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
+    Release("NTSC-J Rev 1/Rev 2 99-06-02", "SLPM-86192", 0xEB733CAA, 0xB91C, 2074, DIRS_NTSC, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
+    Release("PAL 99-06-07", "SLES-01514", 0x337E4A60, 0xB8FC, 2310, DIRS_PAL, FILE_TYPES, Flags.ENCRYPTED_1ST_FOLDER),
 )
 
 FILESIZE_STEP = 256
@@ -134,26 +129,26 @@ def _getChecksum(exe: BinaryIO):
     exe.seek(0)
     return crc32(exe.read(4096))
 
-def _detectRegion(checksum: int, name: str) -> Region:
-    for region in REGIONS:
-        if checksum == region.checksum:
-            logging.info(f"Determined the region as {region.id} ({region.name})")
-            return region
-    logging.error(f"Executable {name} returned an unrecognized checksum! [{checksum:08X}]")
-    logging.error("\tIt is not a valid Silent Hill executable.")
+def _detectRelease(checksum: int, name: str) -> Release:
+    for release in RELEASES:
+        if checksum == release.checksum:
+            logging.info(f"Determined the release as {release.id} ({release.name})")
+            return release
+    logging.error(f"Executable {name} has an unrecognized checksum! [{checksum:08X}]")
+    logging.error("\tIt is not a supported Silent Hill executable.")
     return None
 
-def _parseEntry(entry, region):
+def _parseEntry(entry, release):
     meta, file1, file2 = ENTRY_STRUCT.unpack(entry)
     
-    if not region.flags & Flags.ALT_FILE_STRUCT:
+    if not release.flags & Flags.ALT_FILE_STRUCT:
         name = "".join(chain(
             ( chr(32 + ((file1 >> shift) & 63)) for shift in range(4, 28, 6) ),
             ( chr(32 + ((file2 >> shift) & 63)) for shift in range(0, 24, 6) )
         )).strip()
 
         # size, lba, name, path, type
-        return meta >> 19, meta & 0x7FFFF, name, region.dirs[file1 & 15], region.filetypes[(file2 >> 24) & 15]
+        return meta >> 19, meta & 0x7FFFF, name, release.dirs[file1 & 15], release.filetypes[(file2 >> 24) & 15]
     else:
         directoryIndex0 = (meta >> 31) & 0x1
         directoryIndex1 = file1 & 0x7
@@ -172,13 +167,13 @@ def _parseEntry(entry, region):
         ).strip()
 
         # size, lba, name, path, type
-        return (meta >> 19) & 0xFFF, meta & 0x7FFFF, name, region.dirs[directoryIndex], region.filetypes[extensionIndex]
+        return (meta >> 19) & 0xFFF, meta & 0x7FFFF, name, release.dirs[directoryIndex], release.filetypes[extensionIndex]
 
-def _formatEntry(size, lba, name, path, type, region):
+def _formatEntry(size, lba, name, path, type, release):
     name = name.ljust(8)
     namesep = ','.join(f"'{name[i]}'" for i in range(0, len(name)))
 
-    return f'{{ {lba:#07x}, {size:4d}, {region.dirs.index(path):2d}, FN({namesep}), {region.filetypes.index(type):2d} }}'
+    return f'{{ {lba:#07x}, {size:4d}, {release.dirs.index(path):2d}, FN({namesep}), {release.filetypes.index(type):2d} }}'
         
 
 def _decryptOverlay(data: bytes):
@@ -193,7 +188,7 @@ def _decryptOverlay(data: bytes):
 
     return output
 
-def _extract(entries:Iterable[TableEntry], output: Path, file: BinaryIO, sectorSize: int, regionFlags: int):
+def _extract(entries:Iterable[TableEntry], output: Path, file: BinaryIO, sectorSize: int, releaseFlags: int):
     index = 0
     for i in entries:
         outputPath = (output / i.path).absolute()
@@ -212,7 +207,7 @@ def _extract(entries:Iterable[TableEntry], output: Path, file: BinaryIO, sectorS
         else:
             size = -1 # read until end of file
         data = file.read(size)
-        if i.type == "BIN" and (regionFlags & Flags.ENCRYPTED_1ST_FOLDER):
+        if i.type == "BIN" and (releaseFlags & Flags.ENCRYPTED_1ST_FOLDER):
             if i.path.startswith("1ST"):
                 logging.info("\tDecrypting Overlay...")
                 data = _decryptOverlay(data)
@@ -230,21 +225,21 @@ def main():
         logging.info(f"Checksum of {executable.name}: {checksum:08X}")
         return
     else:
-        region = _detectRegion(checksum, executable.name)
-        if region == None:
+        release = _detectRelease(checksum, executable.name)
+        if release == None:
             return
 
-    originText = f"// Generated from {region.name} ({region.id})\n"
+    originText = f"// Generated from {release.name} ({release.id})\n"
     headerText = originText
     enumText = f"    {originText}"
     entriesSilent = []
     entriesHill = []
-    executable.seek(region.tocOffset)
-    for i in range(region.fileCount):
+    executable.seek(release.tocOffset)
+    for i in range(release.fileCount):
         rawEntry = executable.read(ENTRY_STRUCT.size)
-        size, lba, name, directory, type = _parseEntry(rawEntry, region)
+        size, lba, name, directory, type = _parseEntry(rawEntry, release)
         fullpath = os.path.join(directory, f"{name}.{type}" if not type == "" else f"{name}").replace("\\", "/")
-        headerText += f"/* {i:4d} */ {_formatEntry(size, lba, name, directory, type, region)}, // {fullpath}\n"
+        headerText += f"/* {i:4d} */ {_formatEntry(size, lba, name, directory, type, release)}, // {fullpath}\n"
         enumName = fullpath.replace("/", "_").replace("\\", "_").replace(".", "_")
         enumText += f"    FILE_{enumName} = {i}, // {fullpath}\n"
         entry = TableEntry(fullpath, type, size, lba)
@@ -256,10 +251,10 @@ def main():
                 entriesSilent.append(entry)
     executable.close
     
-    _extract(entriesSilent, args.outputFolder, args.silentFile, 2048, region.flags)
+    _extract(entriesSilent, args.outputFolder, args.silentFile, 2048, release.flags)
     
-    if not region.flags & Flags.NO_XA_CONTAINER and args.hillFile:
-        _extract(entriesHill, args.outputFolder, args.hillFile, 2336, region.flags)
+    if not release.flags & Flags.NO_XA_CONTAINER and args.hillFile:
+        _extract(entriesHill, args.outputFolder, args.hillFile, 2336, release.flags)
 
     with open(os.path.join(args.outputFolder, "filetable.c.inc"), "a+") as f:
         f.truncate(0)
