83bedb7f4c870f2009135ce1bb2207934db8bfd3
emoose <abc@cock.li>
2025-04-23 18:07:19 +0100

    maps: use SQUARE macro, formatting fixups

--- include/maps/shared/sharedFunc_800CBBBC_0_s00.h
+++ include/maps/shared/sharedFunc_800CBBBC_0_s00.h
@@ -4,8 +4,8 @@ s32 sharedFunc_800CBBBC_0_s00()
     s32 var_v1;
 
     distance = SquareRoot0(
-        (ABS_DIFF(sharedData_800E323C_0_s00.vx, sharedData_800E324C_0_s00.vx) * ABS_DIFF(sharedData_800E323C_0_s00.vx, sharedData_800E324C_0_s00.vx)) +
-        (ABS_DIFF(sharedData_800E323C_0_s00.vz, sharedData_800E324C_0_s00.vz) * ABS_DIFF(sharedData_800E323C_0_s00.vz, sharedData_800E324C_0_s00.vz)));
+        (SQUARE(ABS_DIFF(sharedData_800E323C_0_s00.vx, sharedData_800E324C_0_s00.vx))) +
+        (SQUARE(ABS_DIFF(sharedData_800E323C_0_s00.vz, sharedData_800E324C_0_s00.vz))));
 
     var_v1 = ABS_DIFF(sharedData_800E3260_0_s00, sharedData_800E0C6E_0_s00);
 
