49b98bea775eea3f4c629d3dec67e481a30a2e86
emoose <abc@cock.li>
2025-06-10 10:49:56 +0100

    fixups

--- tools/prebuild.sh
+++ tools/prebuild.sh
@@ -9,9 +9,9 @@ sed -i 's/\.a:/\//g' linkers/screens/stream.ld
 
 # .bss section includes 16 random padding bytes, which prevent us from marking them as .bss
 # We'll replace those bytes with 00 so that build can link properly, postbuild.sh will then patch them back in
-sed -i 's/0x090A0D38/0x00000000/g' asm/bodyprog/data/bodyprog_A24CC.bss.s
 sed -i 's/0x622E090A/0x00000000/g' asm/bodyprog/data/bodyprog_A23DC.bss.s
+sed -i 's/0x090A0D38/0x00000000/g' asm/bodyprog/data/bodyprog_A24CC.bss.s
 sed -i 's/0x2E090A0D/0x00000000/g' asm/bodyprog/data/bodyprog_A28B0.bss.s
 sed -i 's/0x61783009/0x00000000/g' asm/bodyprog/data/bodyprog_A28B0.bss.s
 
-sed -i 's/0xAC4073E4/0x00000000/g' asm/screens/stream/data/stream_unk.bss.s
+sed -i 's/0xAC4073E4/0x00000000/g' asm/screens/stream/data/stream_12AE4.bss.s
