f2a96dc6a3ccce406cd488f8a25f0fa924d22bea
Vitalii <vitalii427@yahoo.com>
2025-06-11 20:01:40 +0300

    Reapplied sharedFunc_800D92AC_0_s00 to other maps using maptool.py

--- include/maps/shared/sharedFunc_800D92AC_0_s00.h
+++ include/maps/shared/sharedFunc_800D92AC_0_s00.h
@@ -56,31 +56,8 @@ static inline s32 GetYIndex(s32 x, u32 y)
 
 u8 sharedFunc_800D92AC_0_s00(s32 x, s32 y)
 {
-    #if defined(MAP0_S00)
-        extern u8 D_800DF2DC[];
-        extern u8 D_800DF1FC[];
-
-        #define PRIMARY_GRID_LT  D_800DF2DC
-        #define FALLBACK_GRID_LT D_800DF1FC
-    #elif defined(MAP0_S01)
-        extern u8 D_800DDD78[];
-        extern u8 D_800DDC98[];
-
-        #define PRIMARY_GRID_LT  D_800DDD78
-        #define FALLBACK_GRID_LT D_800DDC98
-    #elif defined(MAP2_S00)
-        extern u8 D_800F06B8[];
-        extern u8 D_800F05D8[];
-
-        #define PRIMARY_GRID_LT  D_800F06B8
-        #define FALLBACK_GRID_LT D_800F05D8
-    #elif defined(MAP2_S03)
-        extern u8 D_800CCC90[];
-        extern u8 D_800CCBB0[];
-
-        #define PRIMARY_GRID_LT  D_800CCC90
-        #define FALLBACK_GRID_LT D_800CCBB0
-    #endif
+    extern u8 sharedData_800DF2DC_0_s00[];
+    extern u8 sharedData_800DF1FC_0_s00[];
 
     s32 xIdx;
     s32 yIdx;
@@ -104,7 +81,7 @@ u8 sharedFunc_800D92AC_0_s00(s32 x, s32 y)
         // Determine XY indices in primary grid.
         xIdx = GetXIndex(x);
         yIdx = GetYIndex(x, y);
-        res  = PRIMARY_GRID_LT[(xIdx * 5) + yIdx];
+        res  = sharedData_800DF2DC_0_s00[(xIdx * 5) + yIdx];
     }
     
     // If no match in primary grid, try fallback grid.
@@ -125,7 +102,7 @@ u8 sharedFunc_800D92AC_0_s00(s32 x, s32 y)
 
         x   = (x + FP_METER_REAL(320.0f)) / FP_METER_REAL(40.0f);
         y   = (y + FP_METER_REAL(240.0f)) / FP_METER_REAL(40.0f);
-        res = FALLBACK_GRID_LT[(x * 16) + y];
+        res = sharedData_800DF1FC_0_s00[(x * 16) + y];
         if (res == 0)
         {
             res = 26;
