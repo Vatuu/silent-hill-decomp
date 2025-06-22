31a899307bacaa3fa6cdce2221014b14666ab3ca
emoose <abc@cock.li>
2025-06-22 17:48:19 +0100

    maps: minor fixups

--- include/maps/shared/sharedFunc_800D0CB8_0_s00.h
+++ include/maps/shared/sharedFunc_800D0CB8_0_s00.h
@@ -5,13 +5,14 @@ void sharedFunc_800D0CB8_0_s00()
     switch (g_SavegamePtr->mapOverlayId_A4)
     {
         case MapOverlayId_MAP0_S00:
-        case MapOverlayId_MAP0_S01:
+        case MapOverlayId_MAP0_S01: // Unused: checks for MAP0_S01, but map itself doesn't contain this func?
         case MapOverlayId_MAP1_S02:
         case MapOverlayId_MAP1_S03:
         case MapOverlayId_MAP4_S02:
             unkValueDiv4 = sharedData_800E32CC_0_s00 >> 2; // sharedData_800E32CC_0_s00 / 4
 
-            // Could be compiler-optimized range checks, something like `if (x > y && x < y + 0xf)`?
+            // TODO: Could be compiler-optimized range checks, something like `if (x >= y && x <= (y + 0xf))`?
+            // (https://wiki.decomp.dev/en/resources/guides/assembly-patterns#range-check)
             if ((sharedData_800E32CC_0_s00 - sharedData_800DD58C_0_s00) > 0xF)
             {
                 sharedData_800E32CC_0_s00 -= 0xF;
@@ -33,7 +34,7 @@ void sharedFunc_800D0CB8_0_s00()
             }
             else if (sharedData_800E32CC_0_s00 != 0)
             {
-                Sd_EngineCmd(0x550U);
+                Sd_EngineCmd(0x550);
                 g_SysWork.field_234B_0 = 1;
             }
     }
