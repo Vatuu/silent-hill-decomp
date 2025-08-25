80e86ab98fc6966b9c5b2617d5875d5eceba388c
tomorrowisnew_ <85917161+tomorroisnew@users.noreply.github.com>
2025-08-25 23:09:08 +0800

    Bodyprog: Matched func_80069FFC (#286)
    * Bodyprog: matched func_80069FFC
    
    Co-authored-by: Sezz <Sezzary@users.noreply.github.com>
    
    * Combined s_func_8006A178 to s_800C4590 and cleanup
    
    ---------
    
    Co-authored-by: Sezz <Sezzary@users.noreply.github.com>

--- include/bodyprog/bodyprog.h
+++ include/bodyprog/bodyprog.h
@@ -270,19 +270,6 @@ STATIC_ASSERT_SIZEOF(s_func_800699F8, 12);
 
 typedef struct
 {
-    s32 field_0;
-    s32 field_4;
-    s32 field_8;
-    s32 field_C;
-    s16 field_10;
-    s16 field_12;
-    s8  field_14;
-    s8  unk_15[3];
-    s32 field_18;
-} s_func_8006A178;
-
-typedef struct
-{
     VECTOR3  position_0;
     SVECTOR3 rotation_C;
     s8       field_12;
@@ -1638,8 +1625,11 @@ typedef struct
 {
     VECTOR3 field_0;
     s32     field_C; // Y height?
-    s8      unk10[4];
+    s16     field_10;
+    s16     field_12;
     s8      field_14;
+    s8      unk_15[3];
+    s32     field_18;
 } s_800C4590;
 
 /** Anim switch struct? */
@@ -1656,6 +1646,13 @@ typedef struct _s_800AFBF4
     u8  unk_B;
 } s_800AFBF4;
 STATIC_ASSERT_SIZEOF(s_800AFBF4, 12);
+
+typedef struct
+{
+    VECTOR3  field_0;
+    SVECTOR3 field_C;
+    s8       field_12;
+} s_func_80069FFC;
 
 // ========
 // GLOBALS
@@ -3386,9 +3383,11 @@ void func_800699E4(s_IpdCollisionData* collData);
 /** Getter for 2D point collision? */
 void func_800699F8(s_func_800699F8* coll, s32 posX, s32 posZ);
 
-s32 func_80069B24(VECTOR3* vec0, VECTOR3* vec1, s_SubCharacter* chara);
+s32 func_80069B24(s_800C4590* arg0, VECTOR3* arg1, s_SubCharacter* chara);
 
-void func_8006A178(s_func_8006A178* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4);
+s32 func_80069FFC(s_800C4590* arg0, VECTOR3* arg1, s_SubCharacter* chara);
+
+void func_8006A178(s_800C4590* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4);
 
 s32 func_8006A1A4(s32 arg0, s_SubCharacter* chara, s32 arg2);
 
