da2e7a51e9a771ab910b9101f4e92d79f0c846f7
Sezz <sezzary@outlook.com>
2025-08-29 19:53:20 +1000

    Split `math.h` constants

--- include/bodyprog/math.h
+++ include/bodyprog/math.h
@@ -2,13 +2,7 @@
 #define _BODYPROG_MATH_H
 
 #include <limits.h>
-
-#define PI    3.1415927f
-#define FP_PI (0x5000 / 2)
-
-#define Q4_SHIFT  4  /** Used for: Q27.4 positions. */
-#define Q8_SHIFT  8  /** Used for: Q23.8 collision positions. Q7.8 camera AABBs. */
-#define Q12_SHIFT 12 /** Used for: Q3.12 alphas, angles. Q19.12 meters, timers, trigonometry. */
+#include "bodyprog/math/constants.h"
 
 // ==============
 // GENERAL UTILS
