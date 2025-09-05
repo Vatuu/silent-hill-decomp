2f4727ed1db33c28a0875b33b7012af3ceacc6c0
Sezz <sezzary@outlook.com>
2025-09-05 17:32:16 +1000

    Anim status conversions

--- src/maps/map0_s02/map0_s02_anim_info.c
+++ src/maps/map0_s02/map0_s02_anim_info.c
@@ -3,19 +3,19 @@
 
 s_AnimInfo g_AnimInfos[] =
 {
-    { Anim_Update2, 76, 0, 77, 0, { FP_TIME(10.0f) }, -1, 676 },
-    { Anim_Update0, 77, 0, 77, 0, { FP_TIME(10.0f) }, 676, 677 },
-    { Anim_Update2, 78, 0, 79, 0, { FP_TIME(10.0f) }, -1, 678 },
-    { Anim_Update0, 79, 0, 79, 0, { FP_TIME(10.0f) }, 678, 713 },
-    { Anim_Update2, 80, 0, 81, 0, { FP_TIME(10.0f) }, -1, 718 },
-    { Anim_Update0, 81, 0, 81, 0, { FP_TIME(15.0f) }, 718, 732 },
-    { Anim_Update2, 82, 0, 83, 0, { FP_TIME(10.0f) }, -1, 737 },
-    { Anim_Update0, 83, 0, 83, 0, { FP_TIME(15.0f) }, 737, 751 },
-    { Anim_Update2, 84, 0, 85, 0, { FP_TIME(10.0f) }, -1, 758 },
-    { Anim_Update0, 85, 0, 85, 0, { FP_TIME(15.0f) }, 758, 770 },
-    { Anim_Update2, 86, 0, 87, 0, { FP_TIME(10.0f) }, -1, 777 },
-    { Anim_Update0, 87, 0, 87, 0, { FP_TIME(15.0f) }, 777, 789 },
-    { Anim_Update2, 88, 0, 89, 0, { FP_TIME(10.0f) }, -1, 485 },
-    { Anim_Update0, 89, 0, 89, 0, { FP_TIME(20.0f) }, 485, 502 },
+    { Anim_Update2, ANIM_STATUS(38, false), 0, ANIM_STATUS(38, true), 0, { FP_TIME(10.0f) }, NO_VALUE, 676 },
+    { Anim_Update0, ANIM_STATUS(38, true),  0, ANIM_STATUS(38, true), 0, { FP_TIME(10.0f) }, 676,      677 },
+    { Anim_Update2, ANIM_STATUS(39, false), 0, ANIM_STATUS(39, true), 0, { FP_TIME(10.0f) }, NO_VALUE, 678 },
+    { Anim_Update0, ANIM_STATUS(39, true),  0, ANIM_STATUS(39, true), 0, { FP_TIME(10.0f) }, 678,      713 },
+    { Anim_Update2, ANIM_STATUS(40, false), 0, ANIM_STATUS(40, true), 0, { FP_TIME(10.0f) }, NO_VALUE, 718 },
+    { Anim_Update0, ANIM_STATUS(40, true),  0, ANIM_STATUS(40, true), 0, { FP_TIME(15.0f) }, 718,      732 },
+    { Anim_Update2, ANIM_STATUS(41, false), 0, ANIM_STATUS(41, true), 0, { FP_TIME(10.0f) }, NO_VALUE, 737 },
+    { Anim_Update0, ANIM_STATUS(41, true),  0, ANIM_STATUS(41, true), 0, { FP_TIME(15.0f) }, 737,      751 },
+    { Anim_Update2, ANIM_STATUS(42, false), 0, ANIM_STATUS(42, true), 0, { FP_TIME(10.0f) }, NO_VALUE, 758 },
+    { Anim_Update0, ANIM_STATUS(42, true),  0, ANIM_STATUS(42, true), 0, { FP_TIME(15.0f) }, 758,      770 },
+    { Anim_Update2, ANIM_STATUS(43, false), 0, ANIM_STATUS(43, true), 0, { FP_TIME(10.0f) }, NO_VALUE, 777 },
+    { Anim_Update0, ANIM_STATUS(43, true),  0, ANIM_STATUS(43, true), 0, { FP_TIME(15.0f) }, 777,      789 },
+    { Anim_Update2, ANIM_STATUS(44, false), 0, ANIM_STATUS(44, true), 0, { FP_TIME(10.0f) }, NO_VALUE, 485 },
+    { Anim_Update0, ANIM_STATUS(44, true),  0, ANIM_STATUS(44, true), 0, { FP_TIME(20.0f) }, 485,      502 },
     {} // End of list.
 };
