2bef3b74ca4b0f23606b94a51cf9cc5f03db7f2b
pseregiet <patryk.seregiet@gmail.com>
2025-09-07 14:40:18 +0200

    crack another section

--- src/bodyprog/syswork_functable.c
+++ src/bodyprog/syswork_functable.c
@@ -19,3 +19,15 @@ void (*g_SysStateFuncs[])() =
     SysState_GameOver_Update,
     SysState_GamePaused_Update
 };
+// temporary data move to this file. After all sections are in C
+// we can find a better file for them.
+s32 D_800A9A68 = 0;
+RECT D_800A9A6C = { 320, 256, 160, 240 };
+s32 g_MainMenuState = 0;
+s32 g_MainMenu_SelectedEntry = 2;
+u32 g_MainMenu_VisibleEntryFlags = (1 << MainMenuEntry_Start) | (1 << MainMenuEntry_Option);
+s8 g_Demo_ReproducedCount = 0;
+// 3 bytes of padding.
+s32 g_MainMenu_NewGameSelectedDifficultyIdx = 1;
+s32 g_PrevSavegameCount = 0;
+
