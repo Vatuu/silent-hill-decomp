d955f958f25184de928886dfbeb50660d32863d2
Sezz <sezzary@outlook.com>
2025-09-18 04:07:12 +1000

    Generate `FLOATSTINGER_ANIM_INFOS`

--- include/maps/shared.h
+++ include/maps/shared.h
@@ -1400,6 +1400,7 @@
     { Anim_Update0, ANIM_STATUS(11, true),  false, ANIM_STATUS(11, true), { Q12(10) }, 295, 295 }
 };*/
 
+/** @brief Bloody Lisa character animation indices. */
 typedef enum _BloodyLisaAnim
 {
     BloodyLisaAnim_Still = 0,
@@ -1415,6 +1416,7 @@ typedef enum _BloodyLisaAnim
     { Anim_Update0, ANIM_STATUS(BloodyLisaAnim_Walk,  true),  false, ANIM_STATUS(BloodyLisaAnim_Walk,  true),  { Q12(10) }, 0,        100 }
 };*/
 
+/** @brief Cat character animation indices. */
 typedef enum _CatAnim
 {
     CatAnim_Still                 = 0,
@@ -1569,6 +1571,54 @@ typedef enum _CatAnim
     { Anim_Update0, ANIM_STATUS(24, true), false, ANIM_STATUS(24, true), { Q12(10) }, 260, 296 },
     { Anim_Update2, ANIM_STATUS(25, false), false, ANIM_STATUS(25, true), { Q12(10) }, NO_VALUE, 260 },
     { Anim_Update0, ANIM_STATUS(25, true), false, ANIM_STATUS(24, true), { Q12(0) }, 260, 296 }
+};*/
+
+/** @brief Floatstinger character animation indices. */
+typedef enum _FloatstingerAnim
+{
+    FloatstingerAnim_Still = 0,
+    FloatstingerAnim_Unk1 = 1,
+    FloatstingerAnim_Unk2 = 2,
+    FloatstingerAnim_Unk3 = 3,
+    FloatstingerAnim_Unk4 = 4,
+    FloatstingerAnim_Unk5 = 5,
+    FloatstingerAnim_Unk6 = 6,
+    FloatstingerAnim_Unk7 = 7,
+    FloatstingerAnim_Unk8 = 8,
+    FloatstingerAnim_Unk9 = 9,
+    FloatstingerAnim_Unk10 = 10,
+    FloatstingerAnim_Unk11 = 11,
+    FloatstingerAnim_Unk12 = 12
+} s_FloatstingerAnim;
+
+/*s_AnimInfo FLOATSTINGER_ANIM_INFOS[] =
+{
+    { Anim_Update2, NO_VALUE, false, ANIM_STATUS(FloatstingerAnim_Still, false), { Q12(0) }, NO_VALUE, 0 },
+    { NULL, ANIM_STATUS(FloatstingerAnim_Still, false), false, ANIM_STATUS(FloatstingerAnim_Still, false), { Q12(0) }, 0, 0 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk1, false), false, ANIM_STATUS(FloatstingerAnim_Unk1, true), { Q12(4) }, NO_VALUE, 1 },
+    { Anim_Update0, ANIM_STATUS(FloatstingerAnim_Unk1, true), false, ANIM_STATUS(FloatstingerAnim_Unk9, false), { Q12(32) }, 1, 31 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk2, false), false, ANIM_STATUS(FloatstingerAnim_Unk2, true), { Q12(5) }, NO_VALUE, 32 },
+    { Anim_Update0, ANIM_STATUS(FloatstingerAnim_Unk2, true), false, ANIM_STATUS(FloatstingerAnim_Unk9, false), { Q12(20) }, 32, 57 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk3, false), false, ANIM_STATUS(FloatstingerAnim_Unk3, true), { Q12(10) }, NO_VALUE, 58 },
+    { Anim_Update0, ANIM_STATUS(FloatstingerAnim_Unk3, true), false, ANIM_STATUS(FloatstingerAnim_Unk11, false), { Q12(20) }, 58, 78 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk4, false), false, ANIM_STATUS(FloatstingerAnim_Unk4, true), { Q12(20) }, NO_VALUE, 116 },
+    { Anim_Update0, ANIM_STATUS(FloatstingerAnim_Unk4, true), false, ANIM_STATUS(FloatstingerAnim_Unk7, false), { Q12(0) }, 116, 136 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk5, false), false, ANIM_STATUS(FloatstingerAnim_Unk5, true), { Q12(2) }, NO_VALUE, 85 },
+    { Anim_Update0, ANIM_STATUS(FloatstingerAnim_Unk5, true), false, ANIM_STATUS(FloatstingerAnim_Unk3, false), { Q12(4) }, 85, 115 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk6, false), false, ANIM_STATUS(FloatstingerAnim_Unk6, true), { Q12(20) }, NO_VALUE, 116 },
+    { Anim_Update1, ANIM_STATUS(FloatstingerAnim_Unk6, true), false, ANIM_STATUS(FloatstingerAnim_Unk11, false), { Q12(0) }, 116, 136 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk7, false), false, ANIM_STATUS(FloatstingerAnim_Unk7, true), { Q12(20) }, NO_VALUE, 137 },
+    { Anim_Update1, ANIM_STATUS(FloatstingerAnim_Unk7, true), false, NO_VALUE, { Q12(0) }, 137, 158 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk8, false), false, ANIM_STATUS(FloatstingerAnim_Unk8, true), { Q12(20) }, NO_VALUE, 159 },
+    { Anim_Update1, ANIM_STATUS(FloatstingerAnim_Unk8, true), false, NO_VALUE, { Q12(20) }, 159, 190 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk9, false), false, ANIM_STATUS(FloatstingerAnim_Unk9, true), { Q12(5) }, NO_VALUE, 191 },
+    { Anim_Update1, ANIM_STATUS(FloatstingerAnim_Unk9, true), false, NO_VALUE, { Q12(0) }, 191, 222 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk10, false), false, ANIM_STATUS(FloatstingerAnim_Unk10, true), { Q12(20) }, NO_VALUE, 223 },
+    { Anim_Update1, ANIM_STATUS(FloatstingerAnim_Unk10, true), false, NO_VALUE, { Q12(20) }, 223, 254 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk11, false), false, ANIM_STATUS(FloatstingerAnim_Unk11, true), { Q12(20) }, NO_VALUE, 78 },
+    { Anim_Update1, ANIM_STATUS(FloatstingerAnim_Unk11, true), false, NO_VALUE, { Q12(0) }, 78, 79 },
+    { Anim_Update2, ANIM_STATUS(FloatstingerAnim_Unk12, false), false, ANIM_STATUS(FloatstingerAnim_Unk12, true), { Q12(4) }, NO_VALUE, 1 },
+    { Anim_Update0, ANIM_STATUS(FloatstingerAnim_Unk12, true), false, ANIM_STATUS(FloatstingerAnim_Unk9, false), { Q12(32) }, 1, 31 }
 };*/
 
 /*s_AnimInfo GHOST_CHILD_ALESSA_ANIM_INFOS[] =
