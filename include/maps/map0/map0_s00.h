1a55c79e6ada8e0d7178c065617c9035c6b05e04
pseregiet <patryk.seregiet@gmail.com>
2025-09-27 16:27:04 +0200

    MAP0_S00: match func_800D0394

--- include/maps/map0/map0_s00.h
+++ include/maps/map0/map0_s00.h
@@ -5,12 +5,6 @@
 
 typedef struct
 {
-    VECTOR3 position0_0;
-    VECTOR3 position1_C;
-} s_800E32DC;
-
-typedef struct
-{
     s16 field_0;
     s16 field_2; // Angle.
 } s_800DFB44;
@@ -22,7 +16,29 @@ typedef struct
     u8 field_2;
 } s_800DFB48;
 
-extern s_800E32DC D_800E32DC;
+typedef struct {
+    VECTOR3 field_0;
+    s16 field_C;
+    s16 field_E;
+    s8  field_10;
+    s8  field_11;
+    s8  field_12;
+    s8  field_13;
+} s_800E34FC;
+STATIC_ASSERT_SIZEOF(s_800E34FC, 0x14);
+
+typedef struct {
+    VECTOR3 field_0;
+    s16 field_C;
+    s16 field_E;
+    s16 field_10;
+    s16 field_12;
+    s8  field_14;
+    s8  field_15;
+} s_800E330C;
+STATIC_ASSERT_SIZEOF(s_800E330C, 0x18);
+
+extern VECTOR3 D_800E32DC[4];
 
 /** "STAGE 0-0" string */
 extern char D_800CAE7C[];
@@ -33,9 +49,9 @@ extern s32 D_800E3A30;
 
 extern u8 D_800DD592;
 
-extern s8 D_800DD593;
+extern u8 D_800DD593;
 
-extern s8 D_800DD594;
+extern u8 D_800DD594;
 
 /** Distance. */
 extern q19_12 D_800DFAD0;
@@ -44,9 +60,9 @@ extern s32 D_800DFAD4;
 
 extern q19_12 D_800DFADC;
 
-extern s8 D_800E34EC;
+extern u8 D_800E34EC;
 
-extern s8 D_800E39AC;
+extern u8 D_800E39AC;
 
 extern s_WorldObject_0 D_800E3A5C[2];
 
@@ -89,6 +105,10 @@ extern s_800C4590 D_800E39BC;
 extern u16 D_800E32D4;
 
 extern s32 D_800DFAD8;
+
+extern s_800E330C D_800E330C[20];
+
+extern s_800E34FC D_800E34FC[60];
 
 void func_800CBFB0(void);
 
@@ -203,5 +223,7 @@ void func_800D802C(s_SubCharacter* chara, s_AnmHeader* anmHdr, GsCOORDINATE2* co
 void func_800D8124(s_SubCharacter* chara, GsCOORDINATE2* coord);
 
 bool func_800D8748(s32 animStatus, s_SubCharacter* chara, s32 keyframeIdx0, s32 keyframeIdx1, s32 arg4, s32 pitch);
+
+void func_800D0394(s32 count, VECTOR3* vecs);
 
 #endif
