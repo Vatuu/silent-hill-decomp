23f078d1733d7f8bccb90bf4cd9614f94da99967
pseregiet <patryk.seregiet@gmail.com>
2025-10-10 16:05:26 +0200

    cleanup

--- include/maps/shared/sharedFunc_800D63A4_0_s01.h
+++ include/maps/shared/sharedFunc_800D63A4_0_s01.h
@@ -3,13 +3,10 @@ void sharedFunc_800D63A4_0_s01(s_SubCharacter* chara)
     s16 rot;
     s32 speed;
     s32 field34;
-    s32 sinVal;
-    s32 cosVal;
     s32 temp_s0;
-    s32 v0_4;
-    s32 v0_5;
+    s32 x;
+    s32 z;
     s32 magSq;
-    u8* base;
 
     sharedFunc_800D6EC4_0_s01(chara);
 
@@ -19,13 +16,13 @@ void sharedFunc_800D63A4_0_s01(s_SubCharacter* chara)
 
     chara->headingAngle_3C = rot;
 
-    sharedData_800DE1F0_0_s01.vx = FP_MULTIPLY_PRECISE(speed, Math_Sin(rot), 12);
+    sharedData_800DE1F0_0_s01.vx = FP_MULTIPLY_PRECISE(speed, Math_Sin(rot), Q12_SHIFT);
     sharedData_800DE1F0_0_s01.vy = field34;
-    sharedData_800DE1F0_0_s01.vz = FP_MULTIPLY_PRECISE(speed, Math_Cos(rot), 12);
+    sharedData_800DE1F0_0_s01.vz = FP_MULTIPLY_PRECISE(speed, Math_Cos(rot), Q12_SHIFT);
 
-    sharedData_800DE1E0_0_s01.vx = FP_MULTIPLY_PRECISE(g_DeltaTime0, sharedData_800DE1F0_0_s01.vx, 12);
-    sharedData_800DE1E0_0_s01.vy = FP_MULTIPLY_PRECISE(g_DeltaTime0, sharedData_800DE1F0_0_s01.vy, 12);
-    sharedData_800DE1E0_0_s01.vz = FP_MULTIPLY_PRECISE(g_DeltaTime0, sharedData_800DE1F0_0_s01.vz, 12);
+    sharedData_800DE1E0_0_s01.vx = FP_MULTIPLY_PRECISE(g_DeltaTime0, sharedData_800DE1F0_0_s01.vx, Q12_SHIFT);
+    sharedData_800DE1E0_0_s01.vy = FP_MULTIPLY_PRECISE(g_DeltaTime0, sharedData_800DE1F0_0_s01.vy, Q12_SHIFT);
+    sharedData_800DE1E0_0_s01.vz = FP_MULTIPLY_PRECISE(g_DeltaTime0, sharedData_800DE1F0_0_s01.vz, Q12_SHIFT);
 
     temp_s0 = sharedFunc_800D6A60_0_s01(
         &sharedData_800E2350_0_s01,
@@ -35,7 +32,7 @@ void sharedFunc_800D63A4_0_s01(s_SubCharacter* chara)
         &sharedData_800E21D0_0_s01.field_11C
     );
 
-    chara->properties_E4.player.flags_11C &= 0xDFFFFFFF;
+    chara->properties_E4.player.flags_11C &= ~PlayerFlag_Unk29;
 
     // @hack permuter find.
     temp_s0++;
@@ -43,16 +40,14 @@ void sharedFunc_800D63A4_0_s01(s_SubCharacter* chara)
 
     if (sharedFunc_800D4A80_0_s01(chara) == 3)
     {
-        //base = (u8*)&D_800E22EC - 0x11C;
-        v0_4 = sharedData_800E21D0_0_s01.field_11C.vx;
-        v0_5 = sharedData_800E21D0_0_s01.field_11C.vz;
+        x = sharedData_800E21D0_0_s01.field_11C.vx;
+        z = sharedData_800E21D0_0_s01.field_11C.vz;
 
-        magSq = FP_MULTIPLY_PRECISE(v0_4, v0_4, 12)
-              + FP_MULTIPLY_PRECISE(v0_5, v0_5, 12);
+        magSq = FP_MULTIPLY_PRECISE(x, x, Q12_SHIFT) + FP_MULTIPLY_PRECISE(z, z, Q12_SHIFT);
 
         if (magSq < 0x7D7)
         {
-            chara->properties_E4.player.flags_11C |= 0x20000000;
+            chara->properties_E4.player.flags_11C |= PlayerFlag_Unk29;
         }
     }
 
