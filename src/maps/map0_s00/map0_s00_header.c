334b9eeedef9b372b79cb736e117610c9287cd9c
pseregiet <patryk.seregiet@gmail.com>
2025-10-10 22:43:01 +0200

    MAP0_S00: cleanup header

--- src/maps/map0_s00/map0_s00_header.c
+++ src/maps/map0_s00/map0_s00_header.c
@@ -6,15 +6,11 @@
 #define BLOOD_SPLAT_COUNT_MAX 150
 
 // Undefined functions, unknown signature.
-#define func_800DBE68 (void(*)(void))0x800DBE68
-#define func_800DBF08 (void(*)(void))0x800DBF08
 #define func_800D0E34 (void(*)(void))0x800D0E34
-#define func_800D1C38 (void(*)(void))0x800D1C38
 #define func_800D23EC (void(*)(void))0x800D23EC
 #define func_800D8A00 (void(*)(void))0x800D8A00
 
 #define func_800D9748 (void(*)(void))0x800D9748
-#define func_800DADD4 (void(*)(void))0x800DADD4
 #define func_800DB94C (void(*)(void))0x800DB94C
 
 extern u8 D_800DF754[];
