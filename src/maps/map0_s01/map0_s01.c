e1105cf0bb7cde0d431249c4b8487502fa30f6af
pseregiet <patryk.seregiet@gmail.com>
2025-10-12 23:28:39 +0200

    MAP0_S01: match func_800D5D80

--- src/maps/map0_s01/map0_s01.c
+++ src/maps/map0_s01/map0_s01.c
@@ -786,7 +786,35 @@ INCLUDE_ASM("asm/maps/map0_s01/nonmatchings/map0_s01", func_800D5B10);
 
 INCLUDE_ASM("asm/maps/map0_s01/nonmatchings/map0_s01", func_800D5C90);
 
-INCLUDE_ASM("asm/maps/map0_s01/nonmatchings/map0_s01", func_800D5D80);
+void func_800D5D80(s_SubCharacter* chara)
+{
+    s32 angle0;
+    s32 angle1;
+    s32 idx;
+    s_sharedData_800E21D0_0_s01* base;
+    s_func_800D2E04* src;
+
+    angle0 = func_80080478(&chara->position_18, &chara->properties_E4.unk0.field_F8);
+    angle1 = FP_ANGLE_NORM_S(angle0 - chara->rotation_24.vy);
+    src = &sharedData_800CAA98_0_s01;
+    base = &sharedData_800E21D0_0_s01;
+
+    idx = 0;
+    base->field_B4[idx][2] = src->unk_380[7][0];
+    base->field_B4[idx][1] = src->unk_380[7][1];
+    idx = 1;
+    base->field_B4[idx][2] = src->unk_380[0x14][0];
+    base->field_B4[idx][1] = src->unk_380[0x14][1];
+    idx = 3;
+    base->field_B4[idx][2] = 0;
+    base->field_B4[idx][1] = src->unk_380[0x23][1];
+
+    if (angle1 < 0)
+    {
+        angle1 += FP_ANGLE(0.3f);
+    }
+    sharedFunc_800D5E78_0_s01(chara, angle1 >> 2);
+}
 
 void func_800D5E14(s_SubCharacter* chara)
 {
