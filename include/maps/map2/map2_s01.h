d1a8907b05ac6af5fcd1dfa1e9a27c5f1492db17
pseregiet <patryk.seregiet@gmail.com>
2025-10-21 17:35:18 +0200

    MAP2_S01: func_800CB010

--- include/maps/map2/map2_s01.h
+++ include/maps/map2/map2_s01.h
@@ -15,6 +15,12 @@ extern VECTOR3 g_CamPosTarget;
 extern VECTOR3 g_CamLookAtTarget;
 extern u8 g_MapMsgSoundIdx;
 
+extern s8 D_800CFDE3;
+extern s32 D_800CFDE4;
+extern s32 D_800D053C;
+extern s32 D_800D0540;
+extern s_ParticleVectors D_800D164C;
+extern s_ParticleVectors D_800D167C;
 
 void func_800CE858(s32 arg0);
 
@@ -25,5 +31,7 @@ void func_800CF798(void);
 void func_800CF7C4(void);
 
 void func_800CE88C(void);
+
+void func_800CB010(s32 arg0, s32 arg1, s32 arg2);
 
 #endif
