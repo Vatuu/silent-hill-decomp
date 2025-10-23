d5ca7601fb28fc96730d8759dc956416efdaedd0
pseregiet <patryk.seregiet@gmail.com>
2025-10-23 09:45:04 +0200

    add missing sharedFunc_800CE8B8_0_s02.h

---
+++ include/maps/shared/sharedFunc_800CE8B8_0_s02.h
@@ -0,0 +1,15 @@
+void sharedFunc_800CE8B8_0_s02(void)
+{
+    switch (g_SavegamePtr->mapRoomIdx_A5)
+    {
+        case 5:
+            func_8003640C(6);
+            break;
+        case 18:
+            func_8003640C(!Savegame_EventFlagGet(EventFlag_379) ? 29 : 7);
+            break;
+        case 17:
+            func_8003640C(7);
+            break;
+    }
+}
