9ad08587ed370b4d09d39629f2ca3f9f8e17b00b
pseregiet <patryk.seregiet@gmail.com>
2025-10-24 20:31:43 +0200

    MAP4_S01: func_800CC19C

--- src/maps/map4_s01/map4_s01.c
+++ src/maps/map4_s01/map4_s01.c
@@ -9,7 +9,56 @@ INCLUDE_RODATA("asm/maps/map4_s01/nonmatchings/map4_s01", D_800C9578);
 
 INCLUDE_RODATA("asm/maps/map4_s01/nonmatchings/map4_s01", g_MapOverlayHeader);
 
-INCLUDE_ASM("asm/maps/map4_s01/nonmatchings/map4_s01", func_800CC19C);
+void func_800CC19C(void)
+{
+    s32 i;
+    s32 counterC;
+
+    if (D_800D7F20.field_0 & 1)
+    {
+        D_800D7F20.field_1A = D_800D7F20.field_1C * 2;;
+        D_800D7F20.field_18 = D_800D7F20.field_1A;
+    }
+    else
+    {
+        D_800D7F20.field_1C = (D_800D7F20.field_18 + D_800D7F20.field_1A) >> 1;
+    }
+
+    D_800D7F20.field_16 = D_800D7F20.field_12 - D_800D7F20.field_14;
+    if (D_800D7F20.field_1)
+    {
+        D_800D7F20.field_10 = 0x1000;
+    }
+    else
+    {
+        D_800D7F20.field_10 = 0;
+    }
+    counterC = D_800D7F20.field_C;
+    i = ARRAY_SIZE(D_800D5B20) - 1;
+    do
+    {
+        if (D_800D5B20[i].field_A == 0)
+        {
+            if (D_800D7F20.field_1 == 0)
+            {
+                func_800CC2C4(i, 0);
+            }
+            else
+            {
+                D_800D5B20[i].field_A = 12;
+            }
+
+            counterC--;
+            if (!counterC)
+            {
+                break;
+            }
+        }
+        i--;
+    } while (i >= 0);
+
+    D_800C4414 |= 1;
+}
 
 INCLUDE_ASM("asm/maps/map4_s01/nonmatchings/map4_s01", func_800CC2C4);
 
