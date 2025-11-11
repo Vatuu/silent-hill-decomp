c48b561ee285341105cb01693b4bbd83fe488205
pseregiet <patryk.seregiet@gmail.com>
2025-11-11 11:51:46 +0100

    MAP1_S03: func_800DA7F4

--- src/maps/map1_s03/map1_s03.c
+++ src/maps/map1_s03/map1_s03.c
@@ -259,7 +259,14 @@ void func_800DA630(void) {}
 
 #include "maps/shared/MapEvent_DoorUnlocked.h" // 0x800DA760
 
-INCLUDE_ASM("asm/maps/map1_s03/nonmatchings/map1_s03", func_800DA7F4);
+void func_800DA7F4(void)
+{
+    VECTOR3 sfxPos = { MAP_POINTS[g_MapEventParam->field_5].positionX_0, Q12(-1.2f), MAP_POINTS[g_MapEventParam->field_5].positionZ_8 };
+
+    Player_ItemRemove(InventoryItemId_LibraryReserveKey, 1);
+    Map_MessageWithSfx(37, Sfx_UseKey, &sfxPos); // "Used the Library reverse key"
+    Savegame_MapMarkingSet(MapMarkFlag_AltSchool2F_LibraryReserveArrows);
+}
 
 void func_800DA8B0(void)
 {
