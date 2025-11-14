c06897f6092193de4f0186c5d7252fc14e76ce91
emoose <abc@cock.li>
2025-11-14 03:42:34 +0000

    main: add note/scratch links to memcpy [skip ci]

--- src/main/memcpy.c
+++ src/main/memcpy.c
@@ -1,5 +1,10 @@
 #include "common.h"
 
+/** NOTE: `memcpy` may rely on hand-tuned or inline assembly.
+ * Some have gotten close to matching but still haven't gotten it completely matched.
+ * Matching it precisely isn't really a priority, since this would likely just be replaced down the line.
+ * Closest scratches: https://decomp.me/scratch/4t6xS / https://decomp.me/scratch/NycrP / https://decomp.me/scratch/kHnqu
+ */
 INCLUDE_ASM("asm/main/nonmatchings/memcpy", memcpy);
 
 void nullsub_800120b0(void) {}
