a92b858592a80ff2407f9b1a023a702934094211
pseregiet <patryk.seregiet@gmail.com>
2025-11-16 14:37:08 +0100

    MAP2_S00: func_800DC598
    Co-Authored-By: fgsfds <2997482+fgsfdsfgs@users.noreply.github.com>

--- src/maps/map2_s00/map2_s00.c
+++ src/maps/map2_s00/map2_s00.c
@@ -404,7 +404,33 @@ INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", func_800DC438);
 
 INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", func_800DC50C);
 
-INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", func_800DC598);
+s32 func_800DC598(s_SubCharacter* chara)
+{
+    s32 ret;
+    u8 animStatus;
+
+    ret = 0;
+    animStatus = chara->model_0.anim_4.status_0;
+    if (animStatus == ANIM_STATUS(25, true) || animStatus == ANIM_STATUS(23, true))
+    {
+        if (chara->position_18.vy < (func_80080884(chara->position_18.vx, chara->position_18.vz) - Q12(0.7f)))
+        {
+            if (func_800DC200(chara) && chara->moveSpeed_38 > Q12(1.5f)) 
+            {
+                ret = 2;
+            }
+            else
+            {
+                func_8006F250(sharedData_800E2370_0_s01, chara->position_18.vx, chara->position_18.vz, 0, 0);
+                if (sharedData_800E2370_0_s01[1] < ((chara->position_18.vy + chara->field_C8) - Q12(2.0f)))
+                {
+                    ret = 1;
+                }
+            }
+        }
+    }
+    return ret;
+}
 
 bool func_800DC67C(s_SubCharacter* chara)
 {
