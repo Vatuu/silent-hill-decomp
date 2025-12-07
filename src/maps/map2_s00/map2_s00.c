bdf214ab9e10f8bfe7120ef1cf34ceec8537eda2
pseregiet <patryk.seregiet@gmail.com>
2025-12-07 17:35:22 +0100

    MAP2_S00: func_800D71A4
    Co-authored-by: Bl00D4NGEL <Bl00D4NGEL@users.noreply.github.com>

--- src/maps/map2_s00/map2_s00.c
+++ src/maps/map2_s00/map2_s00.c
@@ -139,7 +139,215 @@ INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", func_800D6A7C);
 
 #include "maps/shared/sharedFunc_800D6E28_2_s00.h" // 0x800D6E28
 
-INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", func_800D71A4);
+void func_800D71A4(s_SubCharacter* chara)
+{
+    s32 temp_a0;
+    s32 rngAngle0;
+    s32 angle0;
+    s32 angleSubtract;
+    s32 switch3;
+    s32 condModelStateIs2;
+    s32 angle1;
+    s32 dmgType;
+    s32 animStatus;
+    bool tmp1;
+    bool tmp2;
+    bool tmp3;
+    bool flag1;
+    bool flag2;
+    bool flag0;
+
+    animStatus = chara->model_0.anim_4.status_0;
+    angle0 = sharedData_800E21D0_0_s01.field_15C;
+
+    tmp3 = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_0;
+    tmp2 = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_1;
+    tmp1 = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_2;
+    flag0 = tmp3;
+    flag1 = tmp2;
+    flag2 = tmp1;
+    temp_a0 = sharedFunc_800DC50C_2_s00(chara);
+    condModelStateIs2 = switch3 = 0;
+    switch (chara->model_0.stateStep_3)
+    {
+        case 0:
+            chara->properties_E4.unk0.properties_120.val32 = Q12(6.0f);
+            /* fallthrough */
+        case 1:
+            sharedFunc_800DE6A8_2_s00(chara, &chara->properties_E4.unk0.pos_104, Q12(1.5f));
+            if (!sharedFunc_800DC3BC_2_s00(chara) || func_80080514() >= FP_ANGLE(72.0f))
+            {
+                chara->model_0.stateStep_3 = 2;
+    
+            }
+            else
+            {
+                chara->model_0.stateStep_3 = 4;
+            }
+            break;
+        case 2:
+            condModelStateIs2 = 1;
+            /* fallthrough */
+        case 3:
+            if (temp_a0)
+            {
+                chara->model_0.stateStep_3 = 4;
+                break;
+            }
+            if (Math_Distance2dGet(&chara->position_18, &chara->properties_E4.unk0.field_F8) < Q12(1.0f))
+            {
+
+                chara->model_0.stateStep_3 = 1;
+            }
+            else if (!chara->properties_E4.unk0.properties_120.val32)
+            {
+                if (condModelStateIs2)
+                {
+                    switch3 = 1;
+                }
+                chara->model_0.stateStep_3 = 0;
+            }
+            else if (flag0 || flag1 || flag2)
+            {
+                chara->model_0.stateStep_3 = 3;
+                if (angle0 > (FP_ANGLE(2.9f)))
+                {
+                    angle1 = func_80080478(&chara->position_18, &chara->properties_E4.unk0.pos_104) - chara->rotation_24.vy;
+                    angle1 = FP_ANGLE_NORM_S(angle1);
+                    if (angle1 < FP_ANGLE(0.0f))
+                    {
+                        angle1 += FP_ANGLE(0.3f);
+                    }
+                    sharedFunc_800D529C_0_s01(chara, Q12(1.5f), (angle1 >> 2) + chara->rotation_24.vy);
+                    chara->properties_E4.unk0.properties_120.val32 = Q12(6.0f);
+                }
+            }
+            break;
+        case 4:
+            if (animStatus == ANIM_STATUS(19, true))
+            {
+                switch3 = 2;
+                chara->model_0.anim_4.status_0 = ANIM_STATUS(27, false);
+                if (flag2 || flag1)
+                {
+                    chara->model_0.stateStep_3 = 7;
+                }
+                else
+                {
+                    chara->model_0.stateStep_3 = 6;
+                }
+    
+            }
+            else if (flag2 || flag1)
+            {
+                chara->model_0.stateStep_3 = 5;
+            }
+            break;
+        case 5:
+            if (animStatus == ANIM_STATUS(19, true))
+            {
+                switch3 = 3;
+                chara->model_0.anim_4.status_0 = ANIM_STATUS(27, false);
+                chara->model_0.stateStep_3 = 7;
+            }
+            break;
+        case 6:
+            switch3 = 3;
+            if (flag2 || flag1)
+            {
+                chara->model_0.stateStep_3 = 7;
+            }
+            else
+            {
+                switch3 = 2;
+            }
+            break;
+        case 7:
+            switch3 = 3;
+            break;
+    }
+    sharedFunc_800D57C8_0_s01(chara);
+    dmgType = Chara_DamageTake(chara, Q12(1.0f));
+    switch (dmgType)
+    {
+        case 0:
+            switch (switch3) 
+            {
+                case 0:
+                case 1:
+                    if (!flag2 && !flag1)
+                    {
+                        if (switch3 == 1)
+                        {
+                            rngAngle0 = func_80080514();
+                            angleSubtract = FP_ANGLE(0.0f);
+                            if (chara->properties_E4.unk0.field_E8_8 == switch3) 
+                            {
+                                angleSubtract = FP_ANGLE(36.0f);
+                            }
+                            if (rngAngle0 < (FP_ANGLE(252.0f) - (angleSubtract * 2))) 
+                            {
+                                chara->model_0.state_2 = 0x1C;
+                                chara->model_0.stateStep_3 = 0;
+                            }
+                        }
+                    }
+                    else 
+                    {
+                        chara->model_0.state_2 = 0x15;
+                        chara->model_0.stateStep_3 = 0;
+                    }
+                    break;
+                case 2:
+                    if (animStatus == ANIM_STATUS(23, true) || animStatus == ANIM_STATUS(25, true)) 
+                    {
+                        chara->model_0.state_2 = 6;
+                        chara->model_0.stateStep_3 = 0;
+                    }
+                    else if (sharedFunc_800DC0A8_2_s00(chara))
+                    {
+                        chara->model_0.anim_4.status_0 = ANIM_STATUS(23, false);
+                    }
+                    break;
+                case 3:
+                    if (animStatus == ANIM_STATUS(23, true) || animStatus == ANIM_STATUS(25, true)) 
+                    {
+                        chara->model_0.state_2 = 7;
+                        chara->model_0.stateStep_3 = 0;
+                    }
+                    else if (sharedFunc_800DC0A8_2_s00(chara))
+                    {
+                        chara->model_0.anim_4.status_0 = ANIM_STATUS(23, false);
+                    }
+                    break;
+            }
+            break;
+        case 1:
+            chara->model_0.state_2 = 0x1D;
+            chara->model_0.stateStep_3 = 0;
+            chara->properties_E4.unk0.flags_11C |= CharaUnk0Flag_Unk3;
+            break;
+        case 2:
+            chara->model_0.state_2 = 0x1E;
+            chara->model_0.stateStep_3 = 0;
+            chara->properties_E4.unk0.flags_11C |= CharaUnk0Flag_Unk3;
+            break;
+        case 3:
+        case 4:
+            chara->model_0.state_2 = 0x20;
+            chara->model_0.stateStep_3 = 0;
+            if (chara->health_B0 <= 0)
+            {
+                chara->properties_E4.unk0.flags_11C |= CharaUnk0Flag_Unk6;
+            }
+            else
+            {
+                chara->properties_E4.unk0.flags_11C |= CharaUnk0Flag_Unk3;
+            }
+            break;
+        }
+
+}
 
 void func_800D756C(s_SubCharacter* chara)
 {
