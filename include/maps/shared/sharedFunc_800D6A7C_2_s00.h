814fc0525df7ab9d8b2821a9178d8c52092a46af
emoose <abc@cock.li>
2025-12-09 12:25:25 +0000

    maps: fixups

--- include/maps/shared/sharedFunc_800D6A7C_2_s00.h
+++ include/maps/shared/sharedFunc_800D6A7C_2_s00.h
@@ -1,29 +1,33 @@
 void sharedFunc_800D6A7C_2_s00(s_SubCharacter* chara)
 {
-    s32 sp10;
-    s32 flag2;
-    s32 flag0;
-    s32 dist0;
-    s32 temp_s5;
-    s32 flag1;
-    s32 angle0;
-    s32 var_s3;
-    s32 var_s7;
     s32 animStatus;
-    s32 tmp1, tmp2, tmp3;
+    s32 switchCond;
+    s32 sp10;
+    bool cond;
+    bool field_14C_0;
+    bool field_14C_1;
+    bool field_14C_2;
+    bool field_14C_0_tmp;
+    bool field_14C_1_tmp;
+    bool field_14C_2_tmp;
+    s32 temp_s5;
+    s32 distFieldF8;
+    s32 angFieldF8;
 
     animStatus = chara->model_0.anim_4.status_0;
-    var_s3     = 0;
+    switchCond = 0;
     sp10       = sharedFunc_800D4A80_0_s01(chara);
-    var_s7     = 0;
-    tmp3       = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_0;
-    tmp2       = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_1;
-    tmp1       = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_2;
-    flag0      = tmp3;
-    flag1      = tmp2;
-    flag2      = tmp1;
+    cond       = false;
+
+    field_14C_0_tmp = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_0;
+    field_14C_1_tmp = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_1;
+    field_14C_2_tmp = sharedData_800E21D0_0_s01.field_14C.bits32.field_14C_2;
+    field_14C_0     = field_14C_0_tmp;
+    field_14C_1     = field_14C_1_tmp;
+    field_14C_2     = field_14C_2_tmp;
 
     temp_s5 = sharedFunc_800DC50C_2_s00(chara);
+    
     switch (chara->model_0.stateStep_3)
     {
         case 64:
@@ -38,16 +42,16 @@ void sharedFunc_800D6A7C_2_s00(s_SubCharacter* chara)
             break;
 
         case 1:
-            var_s7                                         = 1;
+            cond = true;
             chara->properties_E4.unk0.properties_120.val32 = Q12(6.0f);
-            chara->model_0.stateStep_3                     = 2;
+            chara->model_0.stateStep_3 = 2;
 
         case 2:
-            dist0  = Math_Distance2dGet(&chara->position_18, &chara->properties_E4.unk0.field_F8);
-            angle0 = FP_ANGLE_NORM_S(func_80080478(&chara->position_18, &chara->properties_E4.unk0.field_F8) - chara->rotation_24.vy);
+            distFieldF8 = Math_Distance2dGet(&chara->position_18, &chara->properties_E4.unk0.field_F8);
+            angFieldF8  = FP_ANGLE_NORM_S(func_80080478(&chara->position_18, &chara->properties_E4.unk0.field_F8) - chara->rotation_24.vy);
             if (!temp_s5)
             {
-                if ((dist0 < Q12(1.0f) && (angle0 >= FP_ANGLE(-5.0f) && angle0 <= FP_ANGLE(5.0f))) ||
+                if ((distFieldF8 < Q12(1.0f) && (angFieldF8 >= FP_ANGLE(-5.0f) && angFieldF8 <= FP_ANGLE(5.0f))) ||
                     chara->properties_E4.player.flags_11C < 0)
                 {
                     chara->model_0.stateStep_3 = 0;
@@ -58,13 +62,13 @@ void sharedFunc_800D6A7C_2_s00(s_SubCharacter* chara)
                     {
                         chara->model_0.stateStep_3 = 1;
                     }
-                    else if (var_s7 != 0)
+                    else if (cond)
                     {
-                        if (sharedFunc_800DC200_2_s00(chara) && dist0 > Q12(7.0f))
+                        if (sharedFunc_800DC200_2_s00(chara) && distFieldF8 > Q12(7.0f))
                         {
                             chara->model_0.stateStep_3 = 5;
                         }
-                        else if (sharedFunc_800DC3BC_2_s00(chara) && dist0 < Q12(3.0f) && func_80080514() < FP_ANGLE(252.0f))
+                        else if (sharedFunc_800DC3BC_2_s00(chara) && distFieldF8 < Q12(3.0f) && func_80080514() < FP_ANGLE(252.0f))
                         {
                             chara->model_0.stateStep_3 = 3;
                         }
@@ -80,27 +84,27 @@ void sharedFunc_800D6A7C_2_s00(s_SubCharacter* chara)
         case 3:
             if (animStatus == ANIM_STATUS(19, true))
             {
-                var_s3                         = 1;
+                switchCond                         = 1;
                 chara->model_0.anim_4.status_0 = ANIM_STATUS(27, false);
                 chara->model_0.stateStep_3     = 4;
             }
             break;
 
         case 4:
-            var_s3 = 1;
+            switchCond = 1;
             break;
 
         case 5:
             if (animStatus == ANIM_STATUS(19, true))
             {
-                var_s3                         = 2;
+                switchCond                         = 2;
                 chara->model_0.anim_4.status_0 = ANIM_STATUS(18, false);
                 chara->model_0.stateStep_3     = 6;
             }
             break;
 
         case 6:
-            var_s3 = 2;
+            switchCond = 2;
             break;
     }
 
@@ -109,10 +113,10 @@ void sharedFunc_800D6A7C_2_s00(s_SubCharacter* chara)
     switch (Chara_DamageTake(chara, Q12(1.0f)))
     {
         case 0:
-            switch (var_s3)
+            switch (switchCond)
             {
                 case 0:
-                    if ((flag1 | flag0 | flag2) != 0)
+                    if ((field_14C_1 | field_14C_0 | field_14C_2) != 0)
                     {
                         chara->model_0.state_2     = 20;
                         chara->model_0.stateStep_3 = 0;
@@ -122,7 +126,7 @@ void sharedFunc_800D6A7C_2_s00(s_SubCharacter* chara)
                 case 1:
                     if ((animStatus == ANIM_STATUS(23, true)) || (animStatus == ANIM_STATUS(25, true)))
                     {
-                        if ((flag1 | flag0 | flag2) == 0)
+                        if ((field_14C_1 | field_14C_0 | field_14C_2) == 0)
                         {
                             chara->model_0.state_2 = 4;
                         }
@@ -142,7 +146,7 @@ void sharedFunc_800D6A7C_2_s00(s_SubCharacter* chara)
                 case 2:
                     if (sp10 == 3)
                     {
-                        if ((flag1 | flag0 | flag2) == 0)
+                        if ((field_14C_1 | field_14C_0 | field_14C_2) == 0)
                         {
                             chara->model_0.state_2 = 0x21;
                         }
