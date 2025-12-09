7ec7d045216a0b7fb233333a0b3ce9acc893971b
pseregiet <patryk.seregiet@gmail.com>
2025-12-09 09:35:27 +0100

    MAP2_S00: func_800DD534

--- include/maps/map2/map2_s00.h
+++ include/maps/map2/map2_s00.h
@@ -149,4 +149,5 @@ void func_800EE5D0(void);
 
 void func_800EE660(void);
 
+void func_800DD534(s_SubCharacter* chara);
 #endif
--- src/maps/map2_s00/map2_s00.c
+++ src/maps/map2_s00/map2_s00.c
@@ -913,7 +913,13 @@ INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", sharedFunc_800DD2C4_2_s00
 
 #include "maps/shared/sharedFunc_800DD4EC_2_s00.h" // 0x800DD4EC
 
-INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", func_800DD534);
+void func_800DD534(s_SubCharacter* chara)
+{
+    sharedFunc_800DD2C4_2_s00(chara, Q12(-2.2f), 1);
+    sharedFunc_800DEC84_2_s00(chara, Q12(30.0f), (s32) chara->rotation_24.vy);
+    chara->moveSpeed_38 = sharedData_800CAA98_0_s01.unk_380[9][0];
+    chara->properties_E4.unk0.properties_120.val32 = Q12(10.0f);
+}
 
 INCLUDE_ASM("asm/maps/map2_s00/nonmatchings/map2_s00", func_800DD588);
 
