c9453faa1eeb03908b239d343f9cd49eb307a591
Sezz <sezzary@outlook.com>
2025-12-19 14:15:10 +1100

    Formatting

--- include/maps/shared/sharedFunc_800D31D0_0_s01.h
+++ include/maps/shared/sharedFunc_800D31D0_0_s01.h
@@ -1,18 +1,17 @@
 bool sharedFunc_800D31D0_0_s01(s_SubCharacter* chara, VECTOR3* pos, s32 arg2)
 {
-    q3_12 rotY;
-    s32   offsetX;
-    s32   offsetZ;
-    s32   temp_s3;
-    s32   var_s1;
-    s64   roughDist;
-    u64   temp2;
-    s32   temp3;
+    q3_12  rotY;
+    q19_12 offsetX;
+    q19_12 offsetZ;
+    s32    temp_s3;
+    q19_12 var_s1;
+    q51_12 roughDist;
+    q52_12 distMax;
+    s32    idx;
 
-    temp3 = sharedData_800CAA98_0_s01.field_D3C[chara->properties_E4.unk0.field_E8_4][chara->properties_E4.unk0.field_E8_8];
-
-    var_s1  = sharedData_800CAA98_0_s01.field_D24[1][temp3][0];
-    temp_s3 = sharedData_800CAA98_0_s01.field_D24[1][temp3][1];
+    idx     = sharedData_800CAA98_0_s01.field_D3C[chara->properties_E4.unk0.field_E8_4][chara->properties_E4.unk0.field_E8_8];
+    var_s1  = sharedData_800CAA98_0_s01.field_D24[1][idx][0];
+    temp_s3 = sharedData_800CAA98_0_s01.field_D24[1][idx][1];
 
     switch (g_SavegamePtr->gameDifficulty_260)
     {
@@ -30,7 +29,7 @@ bool sharedFunc_800D31D0_0_s01(s_SubCharacter* chara, VECTOR3* pos, s32 arg2)
 
     if (chara->model_0.state_2 == 3 || chara->model_0.state_2 == 9)
     {
-        var_s1 +=  Q12(1.0f);
+        var_s1 += Q12(1.0f);
     }
 
     rotY    = chara->rotation_24.vy;
@@ -38,9 +37,9 @@ bool sharedFunc_800D31D0_0_s01(s_SubCharacter* chara, VECTOR3* pos, s32 arg2)
     offsetZ = pos->vz - (chara->position_18.vz + FP_MULTIPLY_PRECISE(temp_s3, Math_Cos(rotY), Q12_SHIFT));
 
     roughDist = FP_SQUARE_PRECISE(offsetX, Q12_SHIFT) + FP_SQUARE_PRECISE(offsetZ, Q12_SHIFT);
-    temp2 = FP_SQUARE_PRECISE(var_s1, Q12_SHIFT);
-    temp2 = FP_MULTIPLY_PRECISE(temp2, arg2, Q12_SHIFT);
-    if (roughDist > temp2)
+    distMax = FP_SQUARE_PRECISE(var_s1, Q12_SHIFT);
+    distMax = FP_MULTIPLY_PRECISE(distMax, arg2, Q12_SHIFT);
+    if (roughDist > distMax)
     {
         return false;
     }
--- include/maps/shared/sharedFunc_800D4AEC_0_s01.h
+++ include/maps/shared/sharedFunc_800D4AEC_0_s01.h
@@ -1,78 +1,78 @@
-bool sharedFunc_800D4AEC_0_s01(s_SubCharacter* arg0, VECTOR3* arg1, VECTOR3* arg2, VECTOR3* arg3)
+bool sharedFunc_800D4AEC_0_s01(s_SubCharacter* chara, VECTOR3* arg1, VECTOR3* arg2, VECTOR3* arg3)
 {
     s32      sp18;
     VECTOR3* var_s0;
     s32      temp_v1;
     s32      temp_s0;
-    s32      temp_s0_3;
-    s32      temp_s3;
-    s32      temp_s4;
+    q19_12   unkY;
+    q19_12   offsetX;
+    q19_12   offsetZ;
     s32      var_a0;
-    s32      var_s5;
+    s32      i;
     s32      var_v1;
     s32      temp_v0;
     s32      temp_v0_2;
-    s32      vx0;
-    s32      vy0;
-    s32      vz0;
+    q19_12   posX;
+    q19_12   posY;
+    q19_12   posZ;
     s32      temp;
     s32      temp2;
     s32      temp3;
 
     if (arg1 == NULL)
     {
-        var_s0 = &arg0->position_18;
+        var_s0 = &chara->position_18;
     }
     else
     {
         var_s0 = arg1;
     }
 
-    vx0 = var_s0->vx;
-    vy0 = var_s0->vy;
-    vz0 = var_s0->vz;
+    posX = var_s0->vx;
+    posY = var_s0->vy;
+    posZ = var_s0->vz;
 
-    temp_s3 = arg2->vx - vx0;
-    temp_s4 = arg2->vz - vz0;
+    offsetX = arg2->vx - posX;
+    offsetZ = arg2->vz - posZ;
 
-    sharedData_800DE1A0_0_s01.vx = temp_s3;
-    sharedData_800DE1A0_0_s01.vz = temp_s4;
+    sharedData_800DE1A0_0_s01.vx = offsetX;
+    sharedData_800DE1A0_0_s01.vz = offsetZ;
 
-    if (sharedFunc_800D4A80_0_s01(arg0) == 1)
+    if (sharedFunc_800D4A80_0_s01(chara) == 1)
     {
-        var_s5                       = 0;
+        i                            = 0;
         sharedData_800DE1A0_0_s01.vy = 0;
     }
     else
     {
-        var_s5                       = arg2->vy - vy0;
-        sharedData_800DE1A0_0_s01.vy = var_s5;
+        i                            = arg2->vy - posY;
+        sharedData_800DE1A0_0_s01.vy = i;
     }
 
     temp_s0 = g_SysWork.player_4C.chara_0.field_E1_0;
 
     g_SysWork.player_4C.chara_0.field_E1_0 = 0;
 
-    sp18 = func_8006DB3C(&sharedData_800E2330_0_s01, var_s0, &sharedData_800DE1A0_0_s01, arg0);
+    sp18 = func_8006DB3C(&sharedData_800E2330_0_s01, var_s0, &sharedData_800DE1A0_0_s01, chara);
 
     g_SysWork.player_4C.chara_0.field_E1_0 = temp_s0;
-    temp_s3                                = sharedData_800E2330_0_s01.field_4.vx - vx0;
-    temp_s4                                = sharedData_800E2330_0_s01.field_4.vz - vz0;
+    offsetX                                = sharedData_800E2330_0_s01.field_4.vx - posX;
+    offsetZ                                = sharedData_800E2330_0_s01.field_4.vz - posZ;
 
-    func_8006F250(sharedData_800E2370_0_s01, vx0, vz0, temp_s3, temp_s4);
+    func_8006F250(sharedData_800E2370_0_s01, posX, posZ, offsetX, offsetZ);
 
     var_a0    = sharedData_800E2370_0_s01[1];
-    temp_v1   = arg0->field_C8.field_0;
+    temp_v1   = chara->field_C8.field_0;
     temp_v0_2 = 1;
 
-    if (var_s5 < 0 && var_a0 < (vy0 + temp_v1))
+    if (i < 0 && var_a0 < (posY + temp_v1))
     {
         if ((sharedData_800E2330_0_s01.field_4.vy + temp_v1) < var_a0)
         {
             sharedData_800E2330_0_s01.field_4.vy = var_a0 - temp_v1;
         }
     }
-    else if (sharedData_800E2330_0_s01.field_4.vy + temp_v1 < var_a0)
+    else if ((sharedData_800E2330_0_s01.field_4.vy + temp_v1) < var_a0)
     {
         temp = sharedData_800E2370_0_s01[0];
 
@@ -84,21 +84,21 @@ bool sharedFunc_800D4AEC_0_s01(s_SubCharacter* arg0, VECTOR3* arg1, VECTOR3* arg
             }
             else
             {
-                var_v1 = FP_TO(temp, 0xC) / sharedData_800E2330_0_s01.field_14;
+                var_v1 = FP_TO(temp, Q12_SHIFT) / sharedData_800E2330_0_s01.field_14;
             }
 
             sp18  = true;
-            temp3 = FP_MULTIPLY_PRECISE(temp_s3, var_v1, Q12_SHIFT);
-            temp2 = FP_MULTIPLY_PRECISE(var_s5, var_v1, Q12_SHIFT);
+            temp3 = FP_MULTIPLY_PRECISE(offsetX, var_v1, Q12_SHIFT);
+            temp2 = FP_MULTIPLY_PRECISE(i, var_v1, Q12_SHIFT);
 
             sharedData_800E2330_0_s01.field_14   = temp;
             sharedData_800E2330_0_s01.field_0    = temp_v0_2;
-            sharedData_800E2330_0_s01.field_4.vx = temp3 + vx0;
-            sharedData_800E2330_0_s01.field_4.vy = temp2 + vy0;
+            sharedData_800E2330_0_s01.field_4.vx = temp3 + posX;
+            sharedData_800E2330_0_s01.field_4.vy = temp2 + posY;
             sharedData_800E2330_0_s01.field_18   = sharedData_800E2330_0_s01.field_4.vy;
 
-            temp2                                 = FP_MULTIPLY_PRECISE(temp_s4, var_v1, Q12_SHIFT);
-            sharedData_800E2330_0_s01.field_4.vz  = temp2 + vz0;
+            temp2                                 = FP_MULTIPLY_PRECISE(offsetZ, var_v1, Q12_SHIFT);
+            sharedData_800E2330_0_s01.field_4.vz  = temp2 + posZ;
             sharedData_800E2330_0_s01.field_4.vy += 0; // @hack
         }
     }
@@ -111,24 +111,25 @@ bool sharedFunc_800D4AEC_0_s01(s_SubCharacter* arg0, VECTOR3* arg1, VECTOR3* arg
         {
             if (sharedFunc_800D5274_0_s01() < sharedData_800E2330_0_s01.field_18)
             {
-                temp_v0                              = Rng_RandQ12() / 2 + 0x1800;
+                temp_v0                              = (Rng_RandQ12() / 2) + Q12(1.5f);
                 sharedData_800E2330_0_s01.field_4.vy = sharedData_800E2330_0_s01.field_18 - temp_v0;
             }
             else
             {
-                temp_s0_3  = func_80080884(arg0->position_18.vx, arg0->position_18.vz);
-                temp_v0    = Rng_RandQ12() / 2 + 0x1800;
-                temp_s0_3 -= temp_v0;
+                unkY  = func_80080884(chara->position_18.vx, chara->position_18.vz);
+                temp_v0    = Rng_RandQ12() / 2 + Q12(1.5f);
+                unkY -= temp_v0;
 
-                temp_v0                              = Rng_RandQ12() / 2 - 0x400;
-                sharedData_800E2330_0_s01.field_4.vy = arg0->position_18.vy - temp_v0;
+                temp_v0                              = (Rng_RandQ12() / 2) - Q12(0.25f);
+                sharedData_800E2330_0_s01.field_4.vy = chara->position_18.vy - temp_v0;
 
-                if (temp_s0_3 < sharedData_800E2330_0_s01.field_4.vy)
+                if (unkY < sharedData_800E2330_0_s01.field_4.vy)
                 {
-                    sharedData_800E2330_0_s01.field_4.vy = temp_s0_3;
+                    sharedData_800E2330_0_s01.field_4.vy = unkY;
                 }
             }
         }
     }
+
     return !sp18;
 }
