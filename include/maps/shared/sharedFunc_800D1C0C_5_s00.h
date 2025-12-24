8abb87d5562ea376312f35747bc35ca0c0ba2a44
HumanYeeeee <soooos3175@gmail.com>
2025-12-24 22:35:40 +0300

    updated global and sym files

--- include/maps/shared/sharedFunc_800D1C0C_5_s00.h
+++ include/maps/shared/sharedFunc_800D1C0C_5_s00.h
@@ -2,7 +2,7 @@ void sharedFunc_800D1C0C_5_s00(s_SubCharacter* chara)
 {
     Chara_MoveSpeedUpdate(chara, Q12(4.0f));
 
-    if (ANIM_STATUS_IDX_GET(chara->model_0.anim_4.status_0) == 15 && chara->moveSpeed_38 == 0 && Rng_GenerateInt(Rng_Rand16(), 0, 7) == 0)
+    if (ANIM_STATUS_IDX_GET(chara->model_0.anim_4.status_0) == 15 && chara->moveSpeed_38 == 0 && Rng_GenerateInt(0, 7) == 0)
     {
         chara->model_0.state_2         = 7;
         chara->model_0.anim_4.status_0 = ANIM_STATUS(18, false);
--- include/maps/shared/sharedFunc_800D1CE4_5_s00.h
+++ include/maps/shared/sharedFunc_800D1CE4_5_s00.h
@@ -1,17 +1,17 @@
-extern q19_12 D_800DE28C;
+extern q19_12 sharedData_800DE28C_5_s00;
 
 void sharedFunc_800D1CE4_5_s00(s_SubCharacter* chara)
 {
-    if (chara->properties_E4.splitHead.flags_E8 & 1)
+    if (chara->properties_E4.splithead.flags_E8 & 1)
     {
         chara->field_34 += g_DeltaTime2 >> 1;
         if (ANIM_TIME_RANGE_CHECK(chara->model_0.anim_4.time_4, 176, 184))
         {
-            D_800DE28C = Q12(10.0f);
+            sharedData_800DE28C_5_s00 = Q12(10.0f);
         }
         else
         {
-            D_800DE28C = Q12(0.0f);
+            sharedData_800DE28C_5_s00 = Q12(0.0f);
         }
     }
     else
@@ -24,7 +24,7 @@ void sharedFunc_800D1CE4_5_s00(s_SubCharacter* chara)
                 chara->model_0.anim_4.time_4        = Q12(186.0f);
                 chara->model_0.anim_4.keyframeIdx_8 = 186;
             }
-            D_800DE28C = Q12(8.0f);
+            sharedData_800DE28C_5_s00 = Q12(8.0f);
         }
     }
     if (ANIM_TIME_RANGE_CHECK(chara->model_0.anim_4.time_4, 191, 193))
