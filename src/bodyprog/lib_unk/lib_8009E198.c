9d88b3c233fc45a96b9eaecf9c2f10f1eb903f36
laura ann <laura@lauraann.dev>
2025-12-27 18:56:18 -0800

    lib_unk: `func_8009E2D8`

--- src/bodyprog/lib_unk/lib_8009E198.c
+++ src/bodyprog/lib_unk/lib_8009E198.c
@@ -94,7 +94,21 @@ s32 func_8009E2A0(s_SysWork_2514* arg0)
     return ret;
 }
 
-INCLUDE_ASM("asm/bodyprog/nonmatchings/lib_unk/lib_8009E198", func_8009E2D8);
+s32 func_8009E2D8(s_SysWork_2514* arg0)
+{
+    u32 tmp = *(u32*)arg0;
+    s32 ret = arg0->field_0.field_0_18;
+    if (ret)
+    {
+        // @hack bitfield struct assignments don't match here?
+        u32 mask    = ~(1 << 23);
+        tmp        &= mask;
+        mask        = ~(1 << 18);
+        tmp        &= mask;
+        *(u32*)arg0 = tmp;
+    }
+    return ret;
+}
 
 s32 func_8009E310(s_SysWork_2514* arg0, s_SysWork_2514_C* arg1, s32 arg2) // 0x8009E310
 {
