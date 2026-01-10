72d85001f91494ca5efbf4c69044fba57a303431
belek666 <belek666@onet.eu>
2026-01-10 23:01:52 +0100

    map6_s00: func_800EC4B4

--- include/maps/map6/map6_s00.h
+++ include/maps/map6/map6_s00.h
@@ -9,9 +9,39 @@
 #define HAS_PlayerState_Unk133
 #define KEYFRAME_PlayerState_Unk114 956
 
+typedef struct
+{
+    POLY_GT4* field_0;
+    MATRIX    field_4;
+    MATRIX    field_24;
+    SVECTOR   field_44[3];
+    VECTOR3   field_5C;
+    SVECTOR   field_68;
+    CVECTOR   field_70;
+    CVECTOR   field_74[4];
+    s32       field_84;
+    s32       field_88;
+    s16       field_8C;
+    s16       field_8E;
+    u8        field_90[17][17];
+    s32       field_1B4;
+    s32       field_1B8;
+    s32       field_1BC;
+    s32       field_1C0;
+    s32       field_1C4;
+} s_func_800EC4B4;
+
+typedef struct
+{
+    DVECTOR field_0[17][17];
+    s32     field_484[17][17];
+    CVECTOR field_908[17][17][2];
+} s_func_800EC4B4_2;
+
 extern u16     D_800F0038;
 extern s32     D_800F0040;
 extern s32     D_800F0044;
+extern DVECTOR D_800F0084[17][17];
 extern VECTOR3 D_800F0668;
 extern VECTOR3 D_800F0678;
 extern u8      D_800F0684;
--- src/maps/map6_s00/map6_s00.c
+++ src/maps/map6_s00/map6_s00.c
@@ -491,4 +491,342 @@ void func_800EBEE0(void) // 0x800EBEE0
     }
 }
 
-INCLUDE_ASM("maps/map6_s00/nonmatchings/map6_s00", func_800EC4B4);
+static inline s32 inline_sub(s32 arg0, s32 arg1)
+{
+    return arg0 - arg1;
+}
+
+void func_800EC4B4(s32 arg0)
+{
+    s32 posX;
+    s32 posZ;
+    s32 posY;
+    s32 temp_v1_14;
+    s32 var_a2;
+    s32 var_a3;
+    s32 var_a3_2;
+    s32 j;
+    s32 i;
+    s32 var_t0_2;
+    s32 var_t1_2;
+    s32 var_t2_2;
+    s32 var_t3;
+    s32 temp;
+
+    s_func_800EC4B4*   ptr;
+    s_func_800EC4B4_2* ptr2;
+
+    ptr2 = FS_BUFFER_1;
+    ptr  = PSX_SCRATCH;
+
+    posX = FP_FROM(g_SysWork.playerWork_4C.player_0.position_18.vx, Q12_SHIFT);
+    posY = FP_FROM(g_SysWork.playerWork_4C.player_0.position_18.vy, Q12_SHIFT);
+    posZ = FP_FROM(g_SysWork.playerWork_4C.player_0.position_18.vz, Q12_SHIFT);
+
+    ptr->field_5C.vx = posX << 8;
+    ptr->field_5C.vy = posY << 8;
+    ptr->field_5C.vz = posZ << 8;
+
+    func_80049C2C(&ptr->field_4, FP_TO(posX, Q12_SHIFT), FP_TO(posY, Q12_SHIFT), FP_TO(posZ, Q12_SHIFT));
+
+    gte_SetRotMatrix(&ptr->field_4);
+    gte_SetTransMatrix(&ptr->field_4);
+    gte_ReadGeomScreen(&ptr->field_84);
+
+    ptr->field_24      = GsIDMATRIX;
+    ptr->field_24.t[0] = -0xC00;
+    ptr->field_24.t[2] = 0xF000;
+    ptr->field_24.t[1] = 0;
+    ptr->field_68.vx   = 0;
+    ptr->field_68.vy   = -0x1000;
+    ptr->field_68.vz   = 0;
+
+    for (i = 0; i < 287; i += 3)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            Math_SetSVectorFastSum(&ptr->field_44[j], inline_sub((((i + j) % 17) << 8) - 0x1400, (u16)ptr->field_5C.vx),
+                                   -ptr->field_5C.vy, inline_sub(-0x800 - (u16)ptr->field_5C.vz, ((i + j) / 17) << 8));
+        }
+
+        gte_ldv3c(&ptr->field_44[0]);
+        gte_rtpt();
+        gte_stsxy3c(&ptr2->field_0[i / 17][i % 17]);
+        gte_stsz3c(&ptr2->field_484[i / 17][i % 17]);
+    }
+
+    for (i = 288; i < 289; i++)
+    {
+        Math_SetSVectorFastSum(&ptr->field_44[0], inline_sub(((i % 17) << 8) - 0x1400, (u16)ptr->field_5C.vx),
+                               -ptr->field_5C.vy, inline_sub(-0x800 - (u16)ptr->field_5C.vz, (i / 17) << 8));
+
+        gte_ldv0(&ptr->field_44[0]);
+        gte_rtps();
+        gte_stsxy(&ptr2->field_0[i % 17][i / 17]);
+        gte_stsz(&ptr2->field_484[i % 17][i / 17]);
+    }
+
+    for (i = 0; i < 17; i++)
+    {
+        for (j = 0; j < 17; j++)
+        {
+            *(s32*)&ptr->field_44[0] = ((j << 8) - 0x800) & 0xFFFF;
+            ptr->field_44[0].vz      = 0x800 - (i << 8);
+
+            if (arg0 < D_800F0084[j][i].vx * 4)
+            {
+                ptr->field_90[i][j] = 0;
+                ptr->field_1B4      = 0x80;
+            }
+            else if (arg0 >= D_800F0084[j][i].vy * 4)
+            {
+                ptr->field_90[i][j] = 3;
+                ptr->field_1B4      = 0;
+            }
+            else
+            {
+                ptr->field_1B8 = arg0 - D_800F0084[j][i].vx * 4;
+                ptr->field_1BC = (D_800F0084[j][i].vy - D_800F0084[j][i].vx) * 4;
+                ptr->field_1C0 = ptr->field_1BC >> 2;
+
+                if (ptr->field_1B8 < ptr->field_1C0)
+                {
+                    ptr->field_90[i][j] = 0;
+                    ptr->field_1B4      = ((ptr->field_1C0 - ptr->field_1B8) << 7) / ptr->field_1C0;
+                }
+                else
+                {
+                    ptr->field_1C4 = ptr->field_1BC >> 1;
+
+                    if (ptr->field_1B8 < ptr->field_1C4)
+                    {
+                        ptr->field_90[i][j] = 1;
+                        ptr->field_1B4      = ((ptr->field_1C4 - ptr->field_1B8) << 7) / (ptr->field_1C4 - ptr->field_1C0);
+                    }
+                    else
+                    {
+                        ptr->field_1C0 = (ptr->field_1BC * 3) >> 2;
+
+                        if (ptr->field_1B8 < ptr->field_1C0)
+                        {
+                            ptr->field_90[i][j] = 2;
+                            ptr->field_1B4      = ((ptr->field_1C0 - ptr->field_1B8) << 7) / (ptr->field_1C0 - ptr->field_1C4);
+                        }
+                        else
+                        {
+                            ptr->field_90[i][j] = 3;
+                            ptr->field_1BC     -= (ptr->field_1BC - ptr->field_1C0) >> 1;
+
+                            if (ptr->field_1BC < ptr->field_1B8)
+                            {
+                                ptr->field_1B4 = 0;
+                            }
+                            else
+                            {
+                                ptr->field_1B4 = ((ptr->field_1BC - ptr->field_1B8) << 7) / (ptr->field_1BC - ptr->field_1C0);
+                            }
+                        }
+                    }
+                }
+            }
+
+            func_80055ECC(&ptr->field_70, &ptr->field_44, &ptr->field_68, &ptr->field_24);
+
+            ptr2->field_908[i][j][0].r = MIN((ptr->field_70.r * ptr->field_1B4) >> 7, 0xFF);
+            ptr2->field_908[i][j][0].g = MIN((ptr->field_70.g * ptr->field_1B4) >> 7, 0xFF);
+            ptr2->field_908[i][j][0].b = MIN((ptr->field_70.b * ptr->field_1B4) >> 7, 0xFF);
+
+            ptr2->field_908[i][j][1].r = MIN((ptr->field_70.r * (0x80 - ptr->field_1B4)) >> 7, 0xFF);
+            ptr2->field_908[i][j][1].g = MIN((ptr->field_70.g * (0x80 - ptr->field_1B4)) >> 7, 0xFF);
+            ptr2->field_908[i][j][1].b = MIN((ptr->field_70.b * (0x80 - ptr->field_1B4)) >> 7, 0xFF);
+        }
+    }
+
+    ptr->field_0 = (POLY_GT4*)GsOUT_PACKET_P;
+
+    for (i = 0; i < 16; i++)
+    {
+        for (j = 0; j < 16; j++)
+        {
+            ptr->field_88 = (ptr2->field_484[i][j] + ptr2->field_484[i + 1][j] +
+                             ptr2->field_484[i][j + 1] + ptr2->field_484[i + 1][j + 1]) >>
+                            2;
+
+            if (ptr->field_88 + 0x80 < ptr->field_84)
+            {
+                continue;
+            }
+
+            if ((ptr->field_88 + 0x80 >> 3) >= 0x800)
+            {
+                continue;
+            }
+
+            temp = 0x2F;
+
+            for (var_t2_2 = 0; var_t2_2 < 5; var_t2_2++)
+            {
+                for (var_a3 = 0; var_a3 < 2; var_a3++)
+                {
+                    for (var_a2 = 0; var_a2 < 2; var_a2++)
+                    {
+                        if (ptr->field_90[i + var_a3][j + var_a2] == var_t2_2)
+                        {
+                            ptr->field_74[var_a3 * 2 + var_a2].r = ptr2->field_908[i + var_a3][j + var_a2][0].r;
+                            ptr->field_74[var_a3 * 2 + var_a2].g = ptr2->field_908[i + var_a3][j + var_a2][0].g;
+                            ptr->field_74[var_a3 * 2 + var_a2].b = ptr2->field_908[i + var_a3][j + var_a2][0].b;
+                        }
+                        else if ((ptr->field_90[i + var_a3][j + var_a2] + 1) == var_t2_2)
+                        {
+                            ptr->field_74[var_a3 * 2 + var_a2].r = ptr2->field_908[i + var_a3][j + var_a2][1].r;
+                            ptr->field_74[var_a3 * 2 + var_a2].g = ptr2->field_908[i + var_a3][j + var_a2][1].g;
+                            ptr->field_74[var_a3 * 2 + var_a2].b = ptr2->field_908[i + var_a3][j + var_a2][1].b;
+                        }
+                        else
+                        {
+                            ptr->field_74[var_a3 * 2 + var_a2].r = 0;
+                            ptr->field_74[var_a3 * 2 + var_a2].g = 0;
+                            ptr->field_74[var_a3 * 2 + var_a2].b = 0;
+                        }
+                    }
+                }
+
+                if (!(ptr->field_74[0].r == 0 && ptr->field_74[0].g == 0 && ptr->field_74[0].b == 0) ||
+                    !(ptr->field_74[1].r == 0 && ptr->field_74[1].g == 0 && ptr->field_74[1].b == 0) ||
+                    !(ptr->field_74[2].r == 0 && ptr->field_74[2].g == 0 && ptr->field_74[2].b == 0) ||
+                    !(ptr->field_74[3].r == 0 && ptr->field_74[3].g == 0 && ptr->field_74[3].b == 0))
+                {
+                    ptr->field_1B8 = !(var_t2_2 < 2) + !(var_t2_2 < 3);
+                    temp_v1_14     = ptr->field_1B8 + 5;
+
+                    ptr->field_8E = temp_v1_14 | 0x20;
+                    ptr->field_8C = getClut(g_LoadingScreenImg.clutX, g_LoadingScreenImg.clutY + var_t2_2);
+
+                    if (var_t2_2 == 0)
+                    {
+                        var_t0_2 = 0;
+                        var_a3_2 = 0;
+                        var_t3   = 0x2F;
+                        var_t1_2 = temp;
+                    }
+                    else if (var_t2_2 == 4)
+                    {
+                        var_t0_2       = 0;
+                        ptr->field_1B8 = (((u32)i & 7) > 0) + ((i & 7) == 7);
+                        ptr->field_1BC = (((u32)j & 7) > 0) + ((j & 7) == 7);
+
+                        if (ptr->field_1B8 == 1)
+                        {
+                            if (ptr->field_1BC == 1)
+                            {
+                                var_a3_2 = 0;
+                            }
+                            else
+                            {
+                                var_a3_2 = 0x90;
+                            }
+                        }
+                        else
+                        {
+                            if (ptr->field_1BC == 1)
+                            {
+                                var_a3_2 = 0x30;
+                            }
+                            else
+                            {
+                                var_a3_2 = 0x60;
+                            }
+                        }
+
+                        if (ptr->field_1B8 >= 2)
+                        {
+                            var_t3    = -0x2F;
+                            var_t0_2 += 0x2F;
+                        }
+                        else
+                        {
+                            var_t3 = 0x2F;
+                        }
+
+                        if (ptr->field_1BC <= 0)
+                        {
+                            var_t1_2  = -0x2F;
+                            var_a3_2 += 0x2F;
+                        }
+                        else
+                        {
+                            var_t1_2 = 0x2F;
+                        }
+                    }
+                    else
+                    {
+                        if (j & 4)
+                        {
+                            var_a3_2 = 0xBF - ((j & 3) * 0x30);
+                            var_t1_2 = -0x2F;
+                        }
+                        else
+                        {
+                            var_a3_2 = ((j & 3) * 3) * 0x10;
+                            var_t1_2 = 0x2F;
+                        }
+
+                        if (i & 4)
+                        {
+                            var_t0_2 = ((i & 3) * 0x30) + 0x40;
+                            var_t3   = 0x2F;
+                        }
+                        else
+                        {
+                            var_t0_2 = 0xFF - ((i & 3) * 0x30);
+                            var_t3   = -0x2F;
+                        }
+                    }
+
+                    setPolyGT4(ptr->field_0);
+                    setSemiTrans(ptr->field_0, 1);
+
+                    if (var_t2_2 == 4 && ptr->field_1B8 != ptr->field_1BC && ptr->field_1B8 != 1 && ptr->field_1BC != 1)
+                    {
+                        setXY0Fast(ptr->field_0, ptr2->field_0[i][j + 1].vx, ptr2->field_0[i][j + 1].vy);
+                        setXY1Fast(ptr->field_0, ptr2->field_0[i][j].vx, ptr2->field_0[i][j].vy);
+                        setXY2Fast(ptr->field_0, ptr2->field_0[i + 1][j + 1].vx, ptr2->field_0[i + 1][j + 1].vy);
+                        setXY3Fast(ptr->field_0, ptr2->field_0[i + 1][j].vx, ptr2->field_0[i + 1][j].vy);
+
+                        setRGB0Fast(ptr->field_0, ptr->field_74[1].r, ptr->field_74[1].g, ptr->field_74[1].b);
+                        setRGB1Fast(ptr->field_0, ptr->field_74[0].r, ptr->field_74[0].g, ptr->field_74[0].b);
+                        setRGB2Fast(ptr->field_0, ptr->field_74[3].r, ptr->field_74[3].g, ptr->field_74[3].b);
+                        setRGB3Fast(ptr->field_0, ptr->field_74[2].r, ptr->field_74[2].g, ptr->field_74[2].b);
+
+                        *(s32*)&ptr->field_0->u0 = (var_a3_2 + var_t1_2) + (var_t0_2 << 8) + (ptr->field_8C << 16);
+                        *(s32*)&ptr->field_0->u1 = ((var_a3_2 + var_t1_2) - var_t1_2) + (var_t0_2 << 8) + (ptr->field_8E << 16);
+                        *(u16*)&ptr->field_0->u2 = (var_a3_2 + var_t1_2) + ((var_t0_2 + var_t3) << 8);
+                        *(u16*)&ptr->field_0->u3 = ((var_a3_2 + var_t1_2) - var_t1_2) + ((var_t0_2 + var_t3) << 8);
+                    }
+                    else
+                    {
+                        setXY0Fast(ptr->field_0, ptr2->field_0[i][j].vx, ptr2->field_0[i][j].vy);
+                        setXY1Fast(ptr->field_0, ptr2->field_0[i][j + 1].vx, ptr2->field_0[i][j + 1].vy);
+                        setXY2Fast(ptr->field_0, ptr2->field_0[i + 1][j].vx, ptr2->field_0[i + 1][j].vy);
+                        setXY3Fast(ptr->field_0, ptr2->field_0[i + 1][j + 1].vx, ptr2->field_0[i + 1][j + 1].vy);
+
+                        setRGB0Fast(ptr->field_0, ptr->field_74[0].r, ptr->field_74[0].g, ptr->field_74[0].b);
+                        setRGB1Fast(ptr->field_0, ptr->field_74[1].r, ptr->field_74[1].g, ptr->field_74[1].b);
+                        setRGB2Fast(ptr->field_0, ptr->field_74[2].r, ptr->field_74[2].g, ptr->field_74[2].b);
+                        setRGB3Fast(ptr->field_0, ptr->field_74[3].r, ptr->field_74[3].g, ptr->field_74[3].b);
+
+                        *(s32*)&ptr->field_0->u0 = var_a3_2 + (var_t0_2 << 8) + (ptr->field_8C << 16);
+                        *(s32*)&ptr->field_0->u1 = (var_a3_2 + var_t1_2) + (var_t0_2 << 8) + (ptr->field_8E << 16);
+                        *(u16*)&ptr->field_0->u2 = var_a3_2 + ((var_t0_2 + var_t3) << 8);
+                        *(u16*)&ptr->field_0->u3 = (var_a3_2 + var_t1_2) + ((var_t0_2 + var_t3) << 8);
+                    }
+
+                    addPrim(&g_OrderingTable0[g_ActiveBufferIdx].org[(ptr->field_88 + 0x80) >> 3], ptr->field_0);
+                    ptr->field_0++;
+                }
+            }
+        }
+    }
+
+    GsOUT_PACKET_P = (PACKET*)ptr->field_0;
+}
