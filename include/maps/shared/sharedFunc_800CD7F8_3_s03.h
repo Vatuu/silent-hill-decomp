8e1f51a909139d759e3cd66582c44c58d359520a
Sezz <sezzary@outlook.com>
2026-02-19 18:30:13 +1100

    `sharedFunc_800CD7F8_3_s03`: docs

--- include/maps/shared/sharedFunc_800CD7F8_3_s03.h
+++ include/maps/shared/sharedFunc_800CD7F8_3_s03.h
@@ -1,41 +1,48 @@
 void sharedFunc_800CD7F8_3_s03(s_SubCharacter* chara, q19_12 offsetX, q19_12 offsetZ, q19_12 range)
 {
-    q19_12 adjOffsetX;
-    q19_12 adjOffsetZ;
-    q19_12 adjOffsetSqr;
+    q19_12 extraOffsetX;
+    q19_12 extraOffsetZ;
+    q19_12 extraDistSqr;
     q19_12 rootAdjOffsetSqr;
     q19_12 scale;
-    q19_12 scaledAdjX;
-    q19_12 scaledAdjZ;
+    q19_12 scaledExtraOffsetX;
+    q19_12 scaledExtraOffsetZ;
 
-    adjOffsetX = chara->field_D8.offsetX_4 - offsetX;
-    adjOffsetZ = chara->field_D8.offsetZ_6 - offsetZ;
+    // Compute extra offset.
+    extraOffsetX = chara->field_D8.offsetX_4 - offsetX;
+    extraOffsetZ = chara->field_D8.offsetZ_6 - offsetZ;
 
-    adjOffsetSqr = Q12_SQUARE_PRECISE(adjOffsetX) +
-                   Q12_SQUARE_PRECISE(adjOffsetZ);
-    if (adjOffsetSqr > Q12_MULT_PRECISE(range, 4))
+    // Apply extra offset if its distance exceeds a threshold.
+    extraDistSqr = Q12_SQUARE_PRECISE(extraOffsetX) +
+                   Q12_SQUARE_PRECISE(extraOffsetZ);
+    if (extraDistSqr > Q12_MULT_PRECISE(range, 4))
     {
-        rootAdjOffsetSqr = SquareRoot12(adjOffsetSqr);
+        // Compute scale.
+        rootAdjOffsetSqr = SquareRoot12(extraDistSqr);
         scale            = FP_TO(rootAdjOffsetSqr - 128, Q12_SHIFT) / rootAdjOffsetSqr;
 
-        scaledAdjX = scale * adjOffsetX;
-        if (scaledAdjX < Q12(0.0f))
+        // Adjust extra X offset.
+        scaledExtraOffsetX = scale * extraOffsetX;
+        if (scaledExtraOffsetX < Q12(0.0f))
         {
-            scaledAdjX += Q12_CLAMPED(1.0f);
+            scaledExtraOffsetX += Q12_CLAMPED(1.0f);
         }
-        adjOffsetX = FP_FROM(scaledAdjX, Q12_SHIFT);
+        extraOffsetX = FP_FROM(scaledExtraOffsetX, Q12_SHIFT);
 
-        scaledAdjZ = scale * adjOffsetZ;
-        if (scaledAdjZ < Q12(0.0f))
+        // Adjust extra Z offset.
+        scaledExtraOffsetZ = scale * extraOffsetZ;
+        if (scaledExtraOffsetZ < Q12(0.0f))
         {
-            scaledAdjZ += Q12_CLAMPED(1.0f);
+            scaledExtraOffsetZ += Q12_CLAMPED(1.0f);
         }
-        adjOffsetZ = FP_FROM(scaledAdjZ, Q12_SHIFT);
+        extraOffsetZ = FP_FROM(scaledExtraOffsetZ, Q12_SHIFT);
 
-        offsetX += adjOffsetX;
-        offsetZ += adjOffsetZ;
+        // Apply extra offset.
+        offsetX += extraOffsetX;
+        offsetZ += extraOffsetZ;
     }
 
+    // Set translation offset.
     chara->field_D8.offsetX_4 = offsetX;
     chara->field_D8.offsetZ_6 = offsetZ;
 }
