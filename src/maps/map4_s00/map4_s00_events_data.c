8378cd137ebbabb4acf443d22e5eadd95a7857ca
emoose <abc@cock.li>
2026-02-20 01:29:09 +0000

    maps: parse s_EventParams into *_events_data.c
    TODO:
    - not currently linked into each map yet, need to setup splits for
      them
    - a lot of non-enum eventflags in these that need to be added to
      event_flags.h
    - MAP_POINTS array needs to be parsed by going through each of these
      events, and use pointOfInterestIdx_5 + triggerType_4_0 to use correct
      union type for them
    
    script used to parse these (ugly)
    https://gist.github.com/emoose/fa59b510029682558c4542fcfc314374

---
+++ src/maps/map4_s00/map4_s00_events_data.c
@@ -0,0 +1,36 @@
+#include "bodyprog/bodyprog.h"
+
+s_EventParam D_800CCC14[2] = {
+    {
+        .requiredEventFlag_0  = EventFlag_None,
+        .disabledEventFlag_2  = EventFlag_None,
+        .triggerType_4_0      = TriggerType_None,
+        .activationType_4_4   = TriggerActivationType_None,
+        .pointOfInterestIdx_5 = 0,
+        .requiredItemId_6     = InventoryItemId_Unequipped,
+        .unk_7                = { 0 },
+        .sysState_8_0         = SysState_ReadMessage,
+        .eventData_8_5        = 15,
+        .flags_8_13           = 1,
+        .field_8_19           = 0,
+        .field_8_24           = 0,
+        .mapOverlayIdx_8_25   = MapOverlayId_MAP0_S00,
+        .field_8_31           = 0,
+    },
+    {
+        .requiredEventFlag_0  = EventFlag_None,
+        .disabledEventFlag_2  = EventFlag_None,
+        .triggerType_4_0      = TriggerType_EndOfArray,
+        .activationType_4_4   = TriggerActivationType_None,
+        .pointOfInterestIdx_5 = 0,
+        .requiredItemId_6     = InventoryItemId_Unequipped,
+        .unk_7                = { 0 },
+        .sysState_8_0         = SysState_Gameplay,
+        .eventData_8_5        = 0,
+        .flags_8_13           = 0,
+        .field_8_19           = 0,
+        .field_8_24           = 0,
+        .mapOverlayIdx_8_25   = MapOverlayId_MAP0_S00,
+        .field_8_31           = 0,
+    },
+};
