329b9c46080f75a7fac8b5b34b376b6c0b49100d
emoose <abc@cock.li>
2026-05-20 23:03:23 +0100

    map4_s05: move floatstinger to .c
    TODO: make separate split in map4_s05

--- include/maps/characters/floatstinger.h
+++ include/maps/characters/floatstinger.h
@@ -42,6 +42,13 @@ typedef enum _FloatstingerControl
     FloatstingerControl_6    = 6
 } e_FloatstingerControl;
 
+typedef struct
+{
+    s32 field_0[2];
+    s32 field_8[2];
+    s32 field_10;
+} s_func_800D4458;
+
 /*s_AnimInfo FLOATSTINGER_ANIM_INFOS[] = {
     { Anim_BlendLinear, NO_VALUE, false, ANIM_STATUS(FloatstingerAnim_Still, false), { Q12(0) }, NO_VALUE, 0 },
     { NULL, ANIM_STATUS(FloatstingerAnim_Still, false), false, ANIM_STATUS(FloatstingerAnim_Still, false), { Q12(0) }, 0, 0 },
@@ -70,5 +77,59 @@ typedef enum _FloatstingerControl
     { Anim_BlendLinear, ANIM_STATUS(FloatstingerAnim_12, false), false, ANIM_STATUS(FloatstingerAnim_12, true), { Q12(4) }, NO_VALUE, 1 },
     { Anim_PlaybackOnce, ANIM_STATUS(FloatstingerAnim_12, true), false, ANIM_STATUS(FloatstingerAnim_9, false), { Q12(32) }, 1, 31 }
 };*/
+
+extern s_func_800D4458 D_800D780C[];
+extern u8              D_800D7848[];
+extern q19_12          D_800D7858;
+extern q3_12           D_800D785C;
+extern s32             D_800D7860;
+extern s32             D_800D799C;
+extern void          (*D_800D7A04[])(void);
+extern SVECTOR         D_800D7A20[];
+extern SVECTOR         D_800D7A30;
+extern s16             D_800D7A38[];
+extern u8              D_800D7A58[];
+extern SVECTOR         D_800D7A5C[];
+extern GsCOORDINATE2*  D_800DB928;
+extern q3_12           D_800DB898;
+extern s16             D_800DB89A;
+extern q3_12           D_800DB89C;
+extern s16             D_800DB8A8[];
+extern VECTOR3         D_800DB8C8[];
+
+void Floatstinger_Update(s_SubCharacter* floatstinger, s_AnmHeader* anmHdr, GsCOORDINATE2* coords);
+
+void Floatstinger_Init(s_SubCharacter* floatstinger);
+
+void func_800D1968(s_SubCharacter* floatstinger);
+
+void Floatstinger_ControlUpdate(s_SubCharacter* floatstinger);
+
+void func_800D1BF8(s_SubCharacter* floatstinger);
+
+void func_800D2B90(s_SubCharacter* floatstinger);
+
+void func_800D341C(void);
+
+void func_800D3424(s_SubCharacter* floatstinger);
+
+void func_800D3564(s_SubCharacter* floatstinger);
+
+void func_800D35F0(s_SubCharacter* floatstinger);
+
+void func_800D37E8(s_SubCharacter* floatstinger, s_AnmHeader* anmHdr);
+
+void func_800D3AD4(s_SubCharacter* floatstinger);
+
+void func_800D3F84(VECTOR3* floatstinger, s32 arg1, s32 arg2);
+
+void func_800D41F0(s_SubCharacter* floatstinger);
+
+bool func_800D4458(s_SubCharacter* floatstinger, VECTOR3* arg1);
+
+// Returns angle.
+q19_12 func_800D48A4(s_SubCharacter* floatstinger, q3_12 dist, q3_12 headingAngle);
+
+void func_800D4A3C(s_SubCharacter* floatstinger, VECTOR3* pos, q3_12 angle);
 
 #endif
--- include/maps/map4/map4_s05.h
+++ include/maps/map4/map4_s05.h
@@ -19,17 +19,6 @@
 
 #include "maps/shared.h"
 
-typedef struct
-{
-    s32 field_0[2];
-    s32 field_8[2];
-    s32 field_10;
-} s_func_800D4458;
-
-extern q19_12 D_800D7858;
-
-extern q3_12 D_800D785C;
-
 extern s_AnimInfo FLOATSTINGER_ANIM_INFOS[];
 
 extern VECTOR3 g_Cutscene_CameraPositionTarget;
@@ -57,75 +46,10 @@ extern s16 D_800D7D80;
 
 extern u8 D_800D7D82;
 
-extern q3_12 D_800DB898;
-
-extern q3_12 D_800DB89C;
-
-extern s16 D_800DB8A8[];
-
-extern GsCOORDINATE2* D_800DB928;
-
 extern s_WorldObjectPlacement g_WorldObject_Kidn04;
 extern s_WorldObjectPlacement g_WorldObject_Kidn05;
 
 extern u8 D_800DB9B0;
-
-extern void (*D_800D7A04[])(void);
-
-extern SVECTOR D_800D7A20[];
-
-extern SVECTOR D_800D7A30;
-
-extern VECTOR3 D_800DB8C8[];
-
-extern u8 D_800D7848[];
-
-extern s32 D_800D799C;
-
-extern u8 D_800D7A58[];
-
-extern SVECTOR D_800D7A5C[];
-
-extern s_func_800D4458 D_800D780C[];
-
-extern s16 D_800DB89A;
-
-extern s32 D_800D7860;
-
-extern s16 D_800D7A38[];
-
-void Floatstinger_Update(s_SubCharacter* floatstinger, s_AnmHeader* anmHdr, GsCOORDINATE2* coords);
-
-void Floatstinger_Init(s_SubCharacter* floatstinger);
-
-void func_800D1968(s_SubCharacter* floatstinger);
-
-void Floatstinger_ControlUpdate(s_SubCharacter* floatstinger);
-
-void func_800D1BF8(s_SubCharacter* floatstinger);
-
-void func_800D2B90(s_SubCharacter* floatstinger);
-
-void func_800D3424(s_SubCharacter* floatstinger);
-
-void func_800D3564(s_SubCharacter* floatstinger);
-
-void func_800D35F0(s_SubCharacter* floatstinger);
-
-void func_800D37E8(s_SubCharacter* floatstinger, s_AnmHeader* anmHdr);
-
-void func_800D3AD4(s_SubCharacter* floatstinger);
-
-void func_800D3F84(VECTOR3* floatstinger, s32 arg1, s32 arg2);
-
-void func_800D41F0(s_SubCharacter* floatstinger);
-
-bool func_800D4458(s_SubCharacter* floatstinger, VECTOR3* arg1);
-
-// Returns angle.
-q19_12 func_800D48A4(s_SubCharacter* floatstinger, q3_12 dist, q3_12 headingAngle);
-
-void func_800D4A3C(s_SubCharacter* floatstinger, VECTOR3* pos, q3_12 angle);
 
 void func_800D61A4(void);
 
